c587ebfe36f9e772590ccf9d9c41ced1eeae74f0
Kanno Ami (tomixy) <tomix05may@gmail.com>
2022-07-01 15:47:05 +0900

    styling-patchパッケージ内にTS実行環境を構築

---
+++ packages/core/src/components/Text/Text.js
@@ -0,0 +1,5 @@
+import { jsx as _jsx } from "react/jsx-runtime";
+export const Text = (props) => {
+    return _jsx("span", { children: props.text });
+};
+//# sourceMappingURL=Text.js.map
--- packages/styling-patch/src/index.js
+++
@@ -1,7 +0,0 @@
-'use strict';
-
-module.exports = stylingPatch;
-
-function stylingPatch() {
-    // TODO
-}
