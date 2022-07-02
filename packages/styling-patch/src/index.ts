98b6c29cf4ac8f6776ece0c558ef4333ae0a7756
Kanno Ami (tomixy) <tomix05may@gmail.com>
2022-07-02 16:19:28 +0900

    toJSON関数を定義

--- packages/styling-patch/src/index.ts
+++ packages/styling-patch/src/index.ts
@@ -2,9 +2,19 @@ import { css } from 'mdn-data'
 import { groupList } from 'mdn-data/css/definitions.json'
 import alasql from 'alasql'
 import * as shell from 'shelljs'
+import jsonFormat from 'json-format'
 
 const mdnModuleGroupList = groupList.enum
 const { atRules, selectors, properties } = css
+
+const config_jsonFormat = {
+  type: 'space',
+  size: 2
+}
+
+function toJSON (data: object): string {
+  return jsonFormat(data, config_jsonFormat)
+}
 
 //console.log(mdnModuleGroupList)
 //console.log(atRules)
@@ -28,19 +38,18 @@ type keywordModuleObjList = {
   module: Array<string>
 }
 
-function createKeywordModuleObjList(data: cssDataByMdnList): keywordModuleObjList {
+function createKeywordModuleObjList(data: cssDataByMdnList, keywordKind: string): keywordModuleObjList {
   const flatObjList: Array<CssDataByMdn> = alasql("SELECT * FROM ?", [data])
-  //console.log(JSON.stringify(flatObjList))
-  //shell.ShellString(JSON.stringify(flatObjList)).to('./src/tmp.json')
+  shell.cd('tmp')
+  shell.ShellString(toJSON(flatObjList)).to(`${keywordKind}.json`)
   const cssModuleList: keywordModuleObjList = alasql('SELECT _->(0) AS keyword, _->(1)->groups AS module FROM ?', [flatObjList])
   return cssModuleList
 }
 
-const keywordModuleObjList_atRules = createKeywordModuleObjList(atRules)
-const keywordModuleObjList_selectors = createKeywordModuleObjList(selectors)
-const keywordModuleObjList_properties = createKeywordModuleObjList(properties)
+const keywordModuleObjList_atRules = createKeywordModuleObjList(atRules, 'at-rule')
+const keywordModuleObjList_selectors = createKeywordModuleObjList(selectors, 'selector')
+const keywordModuleObjList_properties = createKeywordModuleObjList(properties, 'property')
 
-//console.log(JSON.stringify(keywordModuleObjList_atRules))
-//console.log(JSON.stringify(keywordModuleObjList_selectors))
-//console.log(JSON.stringify(keywordModuleObjList_properties))
-
+//console.log(toJSON(keywordModuleObjList_atRules))
+//console.log(toJSON(keywordModuleObjList_selectors))
+//console.log(toJSON(keywordModuleObjList_properties))
