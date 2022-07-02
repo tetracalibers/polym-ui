8479f07e2facad4d1460c72c2b6082fd2f021a53
Kanno Ami (tomixy) <tomix05may@gmail.com>
2022-07-02 15:14:38 +0900

    mdn-data/cssのjsonファイルからkeywordとmoduleのみ取り出す処理を実装

--- packages/styling-patch/src/index.ts
+++ packages/styling-patch/src/index.ts
@@ -1,6 +1,7 @@
 import { css } from 'mdn-data'
 import { groupList } from 'mdn-data/css/definitions.json'
 import alasql from 'alasql'
+import * as shell from 'shelljs'
 
 const mdnModuleGroupList = groupList.enum
 const { atRules, selectors, properties } = css
@@ -10,7 +11,36 @@ const { atRules, selectors, properties } = css
 //console.log(selectors)
 //console.log(properties)
 
-let query = "SELECT * FROM ?"
-let result = alasql(query, [atRules])
+interface CssDataByMdn {
+  syntax: string
+  groups: Array<string>
+  status: string
+  mdn_url: string
+  [k: string]: string | object | Array<string>
+}
 
-console.log(result)
+type cssDataByMdnList = {
+  [key: string]: CssDataByMdn
+}
+
+type keywordModuleObjList = {
+  keyword: string
+  module: Array<string>
+}
+
+function createKeywordModuleObjList(data: cssDataByMdnList): keywordModuleObjList {
+  const flatObjList: Array<CssDataByMdn> = alasql("SELECT * FROM ?", [data])
+  //console.log(JSON.stringify(flatObjList))
+  //shell.ShellString(JSON.stringify(flatObjList)).to('./src/tmp.json')
+  const cssModuleList: keywordModuleObjList = alasql('SELECT _->(0) AS keyword, _->(1)->groups AS module FROM ?', [flatObjList])
+  return cssModuleList
+}
+
+const keywordModuleObjList_atRules = createKeywordModuleObjList(atRules)
+const keywordModuleObjList_selectors = createKeywordModuleObjList(selectors)
+const keywordModuleObjList_properties = createKeywordModuleObjList(properties)
+
+//console.log(JSON.stringify(keywordModuleObjList_atRules))
+//console.log(JSON.stringify(keywordModuleObjList_selectors))
+//console.log(JSON.stringify(keywordModuleObjList_properties))
+
