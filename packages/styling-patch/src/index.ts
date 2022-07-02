2748c02443416fd56f84aa719161a320c22e648e
Kanno Ami (tomixy) <tomix05may@gmail.com>
2022-07-02 12:47:06 +0900

    alaSQL動作確認

--- packages/styling-patch/src/index.ts
+++ packages/styling-patch/src/index.ts
@@ -1,11 +1,16 @@
 import { css } from 'mdn-data'
 import { groupList } from 'mdn-data/css/definitions.json'
+import alasql from 'alasql'
 
 const mdnModuleGroupList = groupList.enum
 const { atRules, selectors, properties } = css
 
-console.log(mdnModuleGroupList)
+//console.log(mdnModuleGroupList)
+//console.log(atRules)
+//console.log(selectors)
+//console.log(properties)
 
-console.log(atRules)
-console.log(selectors)
-console.log(properties)
+let query = "SELECT * FROM ?"
+let result = alasql(query, [atRules])
+
+console.log(result)
