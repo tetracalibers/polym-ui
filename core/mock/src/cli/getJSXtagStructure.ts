22697b3d5eec63a696d88e54dc0d860163c35db2
Kanno Ami (tomixy) <tomix05may@gmail.com>
2022-07-06 17:00:34 +0900

    [styling-patch] ハードコーディングされた文字列を定数として定義

--- core/mock/src/cli/getJSXtagStructure.ts
+++ core/mock/src/cli/getJSXtagStructure.ts
@@ -1,25 +1,28 @@
 import shell from 'shelljs'
-import { packageDirectory } from 'pkg-dir'
 import * as path from 'path'
-
+import { packageDirectory } from 'pkg-dir'
 const { ls, cat, ShellString } = shell
+
+const JSX_EXTENTION = ['jsx', 'tsx']
+const NODE_MODULES_DIR = 'node_modules'
+const STYLED_JSX_SECOND_EXTENTION = 'styled'
+const STYLE_XML_FILE_EXTENTION = '.style.xml'
 
 ;(async () => {
   const root = await packageDirectory()
   const filePaths = Array.from(ls('-RA', root))
   const jsxFilePaths = filePaths.filter(filePath => {
     const pathParts = filePath.split('/')
-    const isNotNodeModules = pathParts[0] !== 'node_modules'
+    const isNotNodeModules = pathParts[0] !== NODE_MODULES_DIR
 
     const fileName = path.basename(filePath)
     const fileNameParts = fileName.split('.')
     const extension = path.extname(filePath)
-    const jsxExtention = ['jsx', 'tsx']
-    const isJsx = jsxExtention.includes(extension)
+    const isJsx = JSX_EXTENTION.includes(extension)
 
     const [secondExtention] =
       fileNameParts.length > 2 ? fileNameParts.slice(-1).reverse() : ['']
-    const isNotStyled = secondExtention !== 'styled'
+    const isNotStyled = secondExtention !== STYLED_JSX_SECOND_EXTENTION
 
     return isNotNodeModules && isJsx && isNotStyled
   })
@@ -28,7 +31,10 @@ const { ls, cat, ShellString } = shell
     const src = cat(filePath).toString()
     const jsx = src.split(/<\/?StylePatch>/)[1].trim()
     const fileExtension = path.extname(filePath)
-    const styleFilePath = filePath.replace(fileExtension, '.style.xml')
+    const styleFilePath = filePath.replace(
+      fileExtension,
+      STYLE_XML_FILE_EXTENTION
+    )
     new ShellString(jsx).to(styleFilePath)
     return
   })
