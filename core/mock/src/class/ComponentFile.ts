6fe32a302d2affc62c4aec656fcc89a683894777
Kanno Ami (tomixy) <tomix05may@gmail.com>
2022-07-08 19:21:17 +0900

    [mock] ComponentFileクラスの型定義強化と関数型移行

--- core/mock/src/class/ComponentFile.ts
+++ core/mock/src/class/ComponentFile.ts
@@ -1,23 +1,56 @@
-import SourceFile from './SourceFile'
+import { A } from 'ts-toolbelt'
+import _ from 'lodash'
+import shell from 'shelljs'
+const { cat, ShellString, test } = shell
+import SuperExpressive from 'super-expressive'
 
-export default class ComponentFile extends SourceFile {
-  private readonly STYLING_FILE_EXTENSION = '.style.jsx'
-  private readonly STYLED_JSX_SECOND_EXTENSION = 'styled'
-  private readonly ARROW_EXTENSION = ['tsx', 'jsx']
-  private isStyled: boolean
+interface ComponentFile {
+  path: string
+  name: string
+  extension: 'tsx' | 'jsx'
+  subExtension?: string
+  isStyled?: A.Equals<Pick<ComponentFile, 'subExtension'>, 'styled'>
+}
 
-  public constructor(filePath: string) {
-    super(filePath)
-    this.isStyled = this.subExtension === this.STYLED_JSX_SECOND_EXTENSION
+class ComponentFile implements ComponentFile {
+  findSubExtension(): string | undefined {
+    return _.chain(_.split(this.name, '.')).dropRight().last().value()
   }
 
-  public getJsx(): string {
-    const regexp = /<(StylePatch)>(?<jsx>.*?)<\/\1>/
+  get src(): string {
+    if (test('-e', this.path)) {
+      return cat(this.path).toString().replace(/\r?\n/g, '')
+    } else {
+      return ''
+    }
+  }
+
+  // prettier-ignore
+  get jsx(): string {
+    // /<(StylePatch)>(?<jsx>.*?)<\/\1>/
+    const regexp = SuperExpressive()
+      .startOfInput
+        .string('<StylePatch>')
+        .namedCapture('jsx')
+          .anyOf
+            .word
+            .nonWord
+          .end()
+        .end()
+        .string('</StylePatch>')
+      .endOfInput
+      .toRegex()
     const jsx = regexp.exec(this.src)?.groups?.jsx
     return jsx ? jsx.trim() : ''
   }
 
-  public getStylingFilePath(): string {
-    return this.path.replace(this.extension, this.STYLING_FILE_EXTENSION)
+  get stylingFilePath(): string {
+    return this.path.replace(this.extension, '.styled' + this.extension)
+  }
+
+  copyFile(outFilePath: string): void {
+    new ShellString(this.src).to(outFilePath)
   }
 }
+
+export default ComponentFile
