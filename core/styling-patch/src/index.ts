93b15e0a0e70484f96c43759562d4421cc7e0502
Kanno Ami (tomixy) <tomix05may@gmail.com>
2022-07-08 17:31:07 +0900

    [styling-patch] 関数型プログラミング用ツールの導入

--- core/styling-patch/src/index.ts
+++ core/styling-patch/src/index.ts
@@ -2,3 +2,5 @@ import { StylePatchProvider } from './provider/StylePatchProvider'
 import { StylePatch } from './components/StylePatch'
 
 export { StylePatchProvider, StylePatch }
+
+/* テスト環境 -------------------------------------------------------------------- */
