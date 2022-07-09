6b4b2741d41a20ebbf748d4585ddc31e185c0f0f
Kanno Ami (tomixy) <tomix05may@gmail.com>
2022-07-10 01:38:28 +0900

    [mock] 疑似ASTから疑似CSSに変換する処理を実装

--- core/mock/src/command/parser/astFromStyp.ts
+++ core/mock/src/command/parser/astFromStyp.ts
@@ -3,16 +3,6 @@ import * as P from 'parser-ts/Parser'
 import * as C from 'parser-ts/char'
 import { run } from 'parser-ts/code-frame'
 import { pipe } from 'fp-ts/function'
-import * as fp_S from 'fp-ts/string'
-import { getEq } from 'fp-ts/Array'
-const E = getEq(fp_S.Eq)
-import shell from 'shelljs'
-const { ShellString } = shell
-import jsonFormat from 'json-format'
-const config_jsonFormat = {
-  type: 'space',
-  size: 2,
-}
 
 const styp = `<StylePatch>
   <span>
@@ -43,15 +33,13 @@ const styp = `<StylePatch>
 `.replaceAll(/[\r\n\t]/g, '')
 
 interface AstNode {
-  readonly _kind: string
-  readonly _as?: 'html' | 'component'
-  readonly _token?: string
+  readonly kind: string
   readonly value: string
 }
 
 const spaceTrim = P.surroundedBy(S.spaces)
 
-const openingTag: P.Parser<string, AstNode> = pipe(
+const openingComponentTag: P.Parser<string, AstNode> = pipe(
   P.manyTill(
     pipe(
       C.char('<'),
@@ -60,12 +48,25 @@ const openingTag: P.Parser<string, AstNode> = pipe(
     C.char('>')
   ),
   P.map(value => {
-    const tagName = value.slice(1).join('')
-    const isHtml = E.equals([...fp_S.toLowerCase(tagName)], [...tagName])
     return {
-      _kind: 'OPEN_tag',
-      _as: isHtml ? 'html' : 'component',
-      value: tagName,
+      kind: 'OPEN_componentTag',
+      value: value.slice(1).join(''),
+    }
+  })
+)
+
+const openingHtmlTag: P.Parser<string, AstNode> = pipe(
+  P.manyTill(
+    pipe(
+      C.char('<'),
+      P.alt(() => C.lower)
+    ),
+    C.char('>')
+  ),
+  P.map(value => {
+    return {
+      kind: 'OPEN_htmlTag',
+      value: value.slice(1).join(''),
     }
   })
 )
@@ -79,7 +80,7 @@ const closingTag: P.Parser<string, AstNode> = pipe(
     C.char('>')
   ),
   P.map(value => ({
-    _kind: 'CLOSE_tag',
+    kind: 'CLOSE_tag',
     value: value.slice(1).join(''),
   }))
 )
@@ -93,7 +94,7 @@ const cssProperty: P.Parser<string, AstNode> = pipe(
     C.char(':')
   ),
   P.map(value => ({
-    _kind: 'CSS_property',
+    kind: 'CSS_property',
     value: value.join(''),
   }))
 )
@@ -111,7 +112,7 @@ const cssPropertyValue: P.Parser<string, AstNode> = pipe(
     S.string("',")
   ),
   P.map(value => ({
-    _kind: 'CSS_value',
+    kind: 'CSS_value',
     value: value.join('').replaceAll(/\'/g, ''),
   }))
 )
@@ -119,7 +120,7 @@ const cssPropertyValue: P.Parser<string, AstNode> = pipe(
 const startStylePatch: P.Parser<string, AstNode> = pipe(
   S.string('{{'),
   P.map(value => ({
-    _kind: 'START_css',
+    kind: 'START_css',
     value: value,
   }))
 )
@@ -127,7 +128,7 @@ const startStylePatch: P.Parser<string, AstNode> = pipe(
 const endStylePatch: P.Parser<string, AstNode> = pipe(
   S.string('}}'),
   P.map(value => ({
-    _kind: 'END_css',
+    kind: 'END_css',
     value: value,
   }))
 )
@@ -135,7 +136,7 @@ const endStylePatch: P.Parser<string, AstNode> = pipe(
 const startNestCss: P.Parser<string, AstNode> = pipe(
   S.string('{'),
   P.map(value => ({
-    _kind: 'BEGIN_nesting',
+    kind: 'BEGIN_nesting',
     value: value,
   }))
 )
@@ -144,13 +145,14 @@ const endNestCss: P.Parser<string, AstNode> = pipe(
   S.string('},'),
   P.alt(() => C.char('}')),
   P.map(value => ({
-    _kind: 'END_nesting',
-    value: value,
+    kind: 'END_nesting',
+    value: value.replaceAll(',', ''),
   }))
 )
 
 const parser: P.Parser<string, AstNode> = pipe(
-  spaceTrim(openingTag),
+  spaceTrim(openingHtmlTag),
+  P.alt(() => spaceTrim(openingComponentTag)),
   P.alt(() => spaceTrim(closingTag)),
   P.alt(() => spaceTrim(cssProperty)),
   P.alt(() => spaceTrim(cssPropertyValue)),
@@ -161,6 +163,6 @@ const parser: P.Parser<string, AstNode> = pipe(
   P.map(value => value)
 )
 
-const result = run(P.many(parser), styp)
+export const result = run(P.many(parser), styp)
 
-new ShellString(jsonFormat(result, config_jsonFormat)).to('tmp/ast.json')
+//new ShellString(jsonFormat(result, config_jsonFormat)).to('tmp/ast.json')
