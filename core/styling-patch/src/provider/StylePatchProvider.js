b903a9c26d1dd0e0366db30ef86ac80da2fac0eb
Kanno Ami (tomixy) <tomix05may@gmail.com>
2022-07-13 15:33:15 +0900

    [styling-patch] nano-cssのaddon追加

--- core/styling-patch/src/provider/StylePatchProvider.js
+++ core/styling-patch/src/provider/StylePatchProvider.js
@@ -2,13 +2,26 @@ import { createContext, useContext } from 'react'
 import { create } from 'nano-css'
 import { addon as addonVirtual } from 'nano-css/addon/virtual'
 import { addon as addonRule } from 'nano-css/addon/rule'
+import { addon as addonPrefixed } from 'nano-css/addon/prefixer'
+import { addon as addonArray } from 'nano-css/addon/array'
+import { addon as addonGoogleFont } from 'nano-css/addon/googleFont'
+import { addon as addonExtract } from 'nano-css/addon/extract'
 
 export const styp = create({
-  pfx: '_styp_',
+  pfx: 'styp_',
 })
 
 addonRule(styp)
 addonVirtual(styp)
+addonPrefixed(styp)
+addonArray(styp)
+addonGoogleFont(styp)
+
+const isServer = typeof window === 'undefined'
+
+if (isServer) {
+  addonExtract(styp)
+}
 
 const StylePatchContext = createContext()
 
