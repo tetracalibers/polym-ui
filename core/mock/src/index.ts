11c510da1e3505322cda419651888ed081a22ed5
Kanno Ami (tomixy) <tomix05may@gmail.com>
2022-07-15 15:55:18 +0900

    [mock] SyntaxChecker下書き

--- core/mock/src/index.ts
+++ core/mock/src/index.ts
@@ -74,7 +74,7 @@ const rawStyp = `<StylePatch>
     }}
   </span>
 </StylePatch>
-`.replaceAll(/[\r\n\t]/g, '')
+` //.replaceAll(/[\r\n\t]/g, '')
 
 interface AstNode {
   classification:
@@ -98,10 +98,291 @@ const prefix = 'styp_'
 
 /* -------------------------------------------------------------------------- */
 
-const jstoken = Array.from(jsTokens(rawStyp, { jsx: true }), token => token)
-const jstokenJson = jsonFormat(jstoken, config_jsonFormat)
+const tokenSequence = Array.from(
+  jsTokens(rawStyp, { jsx: true }),
+  token => token
+).filter(token => {
+  const { type, value } = token
+  return !value.includes('\n')
+})
+const jstokenJson = jsonFormat(tokenSequence, config_jsonFormat)
 
 new ShellString(jstokenJson).to('tmp/jstoken.json')
+
+/* -------------------------------------------------------------------------- */
+
+import * as STORE from 'fp-ts/Store'
+import * as STATE from 'fp-ts/State'
+import * as TRACED from 'fp-ts/Traced'
+
+interface Token {
+  type: string
+  value: string
+}
+
+interface Parser extends STORE.Store<number, Token> {
+  next: (_: void) => void
+  traced: (_: void) => Token
+  seek: (p: number) => Parser
+}
+
+class TokenSeqParser implements Parser {
+  constructor(pos = 0) {
+    this.pos = pos
+  }
+  peek = (pos: number) => tokenSequence[pos]
+  pos
+  traced = (_: void) => this.peek(this.pos)
+  seek = (p: number) => new TokenSeqParser(p)
+  next = (_: void) => this.seek(this.pos + 1)
+}
+
+type SyntaxToken = {
+  classification: string
+  body: string
+}
+
+import * as syntaxSchemaJson from './syntax/schema.json'
+
+const contextFlow = JSON.parse(JSON.stringify(syntaxSchemaJson))
+
+interface SyntaxSchema {
+  token: 'any' | string[]
+  next: 'any' | string[]
+  state: string
+}
+
+import * as ARRAY from 'fp-ts/Array'
+
+const contextTypes = [
+  'BEGIN_file',
+  'TAG_prop',
+  'BEGIN_tag',
+  'BEGIN_css',
+  'CSS_statement',
+  'END_css',
+  'END_tag',
+  'END_file',
+] as const
+
+type ContextType = typeof contextTypes[number]
+
+interface ParseResult {
+  token: string
+  valid: boolean
+  pos: number
+}
+
+const chain: <E, A, B>(
+  f: (a: A) => STATE.State<E, B>
+) => (fa: STATE.State<E, A>) => STATE.State<E, B> = f => generate => seed => {
+  const [a, seed2] = generate(seed)
+  const [b, seed3] = f(a)(seed2)
+  return [b, seed3]
+}
+
+/**
+  checker: (p: ParseResult[]) => State<ParseResult[], TokenSeqParser>
+  init   : (t: TokenSeqParser) => State<ParseResult[], TokenSeqParser>
+ */
+const syntaxChecker = (contextType: ContextType) => {
+  let validContext = contextFlow[contextType]
+
+  const checker = (prev: ParseResult[]) => (parser: TokenSeqParser) => {
+    const { token: exceptToken } = _.first(validContext) as SyntaxSchema
+    const targetToken = parser.traced().value
+    console.log(exceptToken, targetToken)
+    const nextParser = parser.next()
+    const curr = {
+      token: targetToken,
+      valid: exceptToken === 'any' || exceptToken.includes(targetToken),
+      pos: parser.pos,
+      except: validContext,
+    } as ParseResult
+    return [ARRAY.concat([curr])(prev), nextParser] as [
+      ParseResult[],
+      TokenSeqParser
+    ]
+  }
+  const initializer = checker([] as ParseResult[])
+
+  return chain(checker)(initializer)(new TokenSeqParser())
+}
+
+console.log(syntaxChecker('BEGIN_file'))
+
+/*
+
+import { isLowerCase } from 'is-lower-case'
+
+const state = tokenSequence.map((token: Token) => {
+  const { type, value } = token
+  if (value === '<') {
+    return ['settled', '']
+  }
+  if (value === '>') {
+    return ['pending', '']
+  }
+  return ['settled', value]
+})
+
+import * as ARRAY from 'fp-ts/Array'
+
+console.log(state)
+
+/*
+
+import * as OPTION from 'fp-ts/Option'
+import * as STATE_T from 'fp-ts/StateT'
+
+const evaluate = STATE.evaluate(OPTION.Functor)
+
+import * as CHAIN from 'fp-ts/Chain'
+import type { Applicative2 } from 'fp-ts/lib/Applicative'
+import type { Chain2 } from 'fp-ts/lib/Chain'
+import type { Functor2 } from 'fp-ts/lib/Functor'
+
+const URI2 = 'StateOption'
+type URI2 = typeof URI2
+
+type StateOption<S, A> = STATE_T.StateT1<'Option', S, A>
+
+// 型クラスインスタンスを作るためURItoKindに登録する
+declare module 'fp-ts/lib/HKT' {
+  interface URItoKind2<E, A> {
+    readonly [URI2]: StateOption<E, A>
+  }
+}
+
+// 型クラスのメソッドを作る
+// 'fp-ts/StateT'から提供されてるメソッドにより簡略化できる
+const map = STATE_T.map(OPTION.Functor)
+const ap = STATE_T.ap(OPTION.Chain)
+const of = STATE_T.of(OPTION.Pointed)
+const chain = STATE_T.chain(OPTION.Chain)
+
+// 各インスタンスを作る
+// ST.mapによって作られる`map`とは引数のもらい方が違うため調整する
+const Functor: Functor2<URI2> = {
+  URI: URI2,
+  map: <E, A, B>(ma: StateOption<E, A>, f: (a: A) => B): StateOption<E, B> => {
+    return map(f)(ma)
+  },
+}
+
+// map同様引数を調整する
+const Applicative: Applicative2<URI2> = {
+  ...Functor,
+  of,
+  ap: <E, A, B>(
+    fab: StateOption<E, (a: A) => B>,
+    fa: StateOption<E, A>
+  ): StateOption<E, B> => {
+    return ap(fa)(fab)
+  },
+}
+
+// map同様...
+const Chain: Chain2<URI2> = {
+  ...Applicative,
+  chain: <E, A, B>(
+    fa: StateOption<E, A>,
+    f: (a: A) => StateOption<E, B>
+  ): StateOption<E, B> => {
+    return chain(f)(fa)
+  },
+}
+
+// Chainの制約付きのbindを作る関数`C.bind`を使ってbind関数を作る
+const bind = CHAIN.bind(Chain)
+
+const fromState = STATE_T.fromState(OPTION.Pointed)
+
+const get = <S>() => fromState<S, S>(STATE.get<S>())
+const put = <S>(s: S) => fromState<S, void>(STATE.put<S>(s))
+
+// 与えられた関数の処理を後続する関数に繋がないための関数
+const chainFirst = CHAIN.chainFirst(Chain)
+
+/*
+
+type S = { hello: string }
+console.log(
+  evaluate({ hello: 'world' })(
+    pipe(
+      of<number, S>(1),
+      bind('before', () => {
+        return get()
+      }),
+      // `put`は`State s Option void`を返すため`chain`で繋ぐとそれまでの処理が捨てられるので、`chainFirst`を使う
+      chainFirst(() => {
+        return put({ hello: 'fp-ts' })
+      }),
+      bind('after', () => {
+        return get()
+      })
+    )
+  )
+)
+
+/*
+
+const tokenClassification = (_token: string) => {
+  // token === 'StylePatch'
+  // token === all lowercase
+  // token === capitalize
+  return ''
+}
+
+import * as ARRAY from 'fp-ts/Array'
+
+const astBuildProgress =
+  (currToken: string) =>
+  (classification: string) =>
+  (collection: SyntaxToken[]): [SyntaxToken[], string] => {
+    const currSyntax = {
+      classification: classification,
+      body: currToken,
+    }
+    const newCollection = _.union(collection, [currSyntax])
+    //console.log(newCollection)
+    if (currSyntax.body === '>') {
+      return [newCollection, 'found']
+    } else {
+      return [newCollection, 'continue']
+    }
+  }
+
+const astNodeBuilder = {
+  tag: (
+    syntaxTokenCollection: SyntaxToken[],
+    token: Token
+  ): [SyntaxToken[], string] => {
+    const [collection, status] = astBuildProgress(token.value)(
+      tokenClassification(token.value)
+    )(syntaxTokenCollection)
+    //console.log(collection)
+    return [collection, status]
+  },
+}
+
+const tokenMatcher = (cursor: number, collec = [] as SyntaxToken[]) => {
+  const token = tokenSequenceParser.peek(cursor)
+  //console.log(token)
+  let [updatedCollec, status] = astNodeBuilder.tag(collec, token)
+  //.otherwise(() => [collec, 'continue'])
+  STORE.seek(cursor + 1)(tokenSequenceParser)
+  return match(status)
+    .with('found', () => updatedCollec)
+    .otherwise(() => tokenMatcher(cursor + 1, updatedCollec))
+  //.otherwise(() => [[], []])
+}
+
+const res = tokenSequence.map((_, cursor) => tokenMatcher(cursor))
+
+const resJson = jsonFormat(res, config_jsonFormat)
+
+//console.log(resJson)
 
 /* -------------------------------------------------------------------------- */
 
@@ -257,7 +538,7 @@ const tokens = result._tag === 'Right' ? result.right : []
 
 const json = jsonFormat(tokens, config_jsonFormat)
 
-new ShellString(json).to('tmp/tokens.json')
+new ShellString(json).to('tmp/ast.json')
 
 /* -------------------------------------------------------------------------- */
 
