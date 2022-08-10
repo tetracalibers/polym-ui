c3e5a6fe8e359fbe0dbec6ce5cf5bd9eecea2352
Kanno Ami (tomixy) <tomix05may@gmail.com>
2022-08-10 12:02:56 +0900

    [.storybook] storiesパス変更

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,8 +1,8 @@
 module.exports = {
   stories: [
-    //'../packages/**/*.stories.@(tsx|ts|mdx)',
+    '../packages/**/src/components/*.stories.@(tsx|ts|mdx)',
     //'../core/mock/src/components/**/*.stories.@(tsx|ts|mdx)',
-    '../examples/syntax/src/components/**/*.stories.@(tsx|ts|mdx)',
+    //'../examples/syntax/src/components/**/*.stories.@(tsx|ts|mdx)',
   ],
   addons: [
     '@storybook/addon-links',
