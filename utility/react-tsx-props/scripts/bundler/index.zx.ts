402bc3f0827dbf54185a07cb0fe3361b9de3d62d
Kanno Ami (tomixy) <tomix05may@gmail.com>
2022-08-15 07:13:16 +0900

    [react-tsx-props -> bundler] デバッグモードへの切り替えを簡単に行えるようリファクタリング

--- utility/react-tsx-props/scripts/bundler/index.zx.ts
+++ utility/react-tsx-props/scripts/bundler/index.zx.ts
@@ -2,18 +2,51 @@
 
 /* eslint-disable no-use-before-define */
 /* eslint-disable no-undef */
+
 import 'zx/globals'
+import _ from 'lodash'
 import { bundleResource } from './rollup/rollup.resource.js'
+
+/* -------------------------------------------------------------------------- */
+/* CONFIG                                                                     */
+/* -------------------------------------------------------------------------- */
+
+const DEBUG = true
+
+/* -------------------------------------------------------------------------- */
+/* UTILITIES                                                                  */
+/* -------------------------------------------------------------------------- */
 
 const isEven = (n: number) => n % 2 === 0
 
-const commands = [...new Array(bundleResource.length * 2)].map((_, idx) => {
+/* -------------------------------------------------------------------------- */
+/* COMMAND                                                                    */
+/* -------------------------------------------------------------------------- */
+
+const PREBUILD_FLAGS = [
+  '--config',
+  '--config_type_check_mode',
+  '--config_target_idx',
+]
+
+const BUILD_FLAGS = _.without(PREBUILD_FLAGS, '--config_type_check_mode')
+
+const commandList = [...new Array(bundleResource.length * 2)].map((_, idx) => {
   const targetId = Math.floor(idx / 2)
-  const build = () => $`rollup --config --config_target_idx ${targetId}`
+  const build = () =>
+    DEBUG
+      ? echo`rollup ${BUILD_FLAGS} ${targetId}`
+      : $`rollup ${BUILD_FLAGS} ${targetId}`
   const prebuild = () =>
-    $`rollup --config --config_type_check_mode --config_target_idx ${targetId}`
+    DEBUG
+      ? echo`rollup ${PREBUILD_FLAGS} ${targetId}`
+      : $`rollup ${PREBUILD_FLAGS} ${targetId}`
   return isEven(idx) ? prebuild : build
 })
+
+/* -------------------------------------------------------------------------- */
+/* CORE                                                                       */
+/* -------------------------------------------------------------------------- */
 
 function* traverserGenerator<T>(collec: (() => T)[]) {
   yield* collec
@@ -26,8 +59,6 @@ async function* execCycleGenerator<T>(
   yield
 }
 
-const commandList = traverserGenerator(commands)
-
 const exec = async <T>(
   taskListTraverser: Generator<() => T, void, undefined>
 ): Promise<boolean> => {
@@ -36,4 +67,14 @@ const exec = async <T>(
   return done || exec(taskListTraverser)
 }
 
-exec(commandList)
+/* -------------------------------------------------------------------------- */
+/* EXEC                                                                       */
+/* -------------------------------------------------------------------------- */
+
+const commandListTraverser = traverserGenerator(commandList)
+
+const done = await exec(commandListTraverser)
+
+if (done) {
+  //fs.copy('@types/')
+}
