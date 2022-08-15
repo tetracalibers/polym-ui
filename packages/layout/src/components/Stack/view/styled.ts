353b84ac6692bdae81732e525da5d8418c6d0680
Kanno Ami (tomixy) <tomix05may@gmail.com>
2022-08-15 17:14:28 +0900

    [layout -> Stack] Existユーティリティ関数を定義し、styled記法を整頓

--- packages/layout/src/components/Stack/view/styled.ts
+++ packages/layout/src/components/Stack/view/styled.ts
@@ -1,6 +1,20 @@
-import styled, { css } from 'styled-components'
+import styled, { css, FlattenSimpleInterpolation } from 'styled-components'
 import { StackProps } from './props'
 import { PUT, SELECT } from 'styled-utility-first'
+
+class _Truthy<P> {
+  private _prop: P
+  constructor(prop: P) {
+    this._prop = prop
+  }
+
+  when(cssTemplateLiteral: FlattenSimpleInterpolation) {
+    return this._prop && cssTemplateLiteral
+  }
+}
+
+const Truthy = <P>(prop: P) => new _Truthy<P>(prop)
+const Exist = <P>(prop: P) => new _Truthy<P>(prop)
 
 export const Root = styled.div.attrs<StackProps>(props => ({
   recursive: props.recursive,
@@ -13,10 +27,12 @@ export const Root = styled.div.attrs<StackProps>(props => ({
     justify-content: flex-start;
   }
 
+  // 既存の垂直マージンを削除
   & > * {
     ${PUT.margin.vertical.clear()}
   }
 
+  // 連続する要素にだけ上方向のマージンを適用
   ${({ recursive, space }) => {
     return css`
       ${SELECT.continuousElements({ recursive })} {
@@ -25,14 +41,12 @@ export const Root = styled.div.attrs<StackProps>(props => ({
     `
   }}
 
+  // "separateFrom"より下のボックスを下部にまとめるため隙間を空ける
   ${({ separateFrom }) => {
-    return (
-      separateFrom &&
-      css`
-        & > :nth-child(${separateFrom}) {
-          margin-bottom: auto;
-        }
-      `
-    )
+    return Exist(separateFrom).when(css`
+      & > :nth-child(${separateFrom}) {
+        margin-bottom: auto;
+      }
+    `)
   }}
 `
