39895b4eb7fe93d2501cf045cc01527e98d26933
Kanno Ami (tomixy) <tomix05may@gmail.com>
2022-08-16 18:07:48 +0900

    [styled-utility-first] ベースコンポーネントのスタイリングprops用型定義のグループ化を行う型を作成

--- packages/clickableTrigger/src/button/base/view/styled.ts
+++ packages/clickableTrigger/src/button/base/view/styled.ts
@@ -1,25 +1,58 @@
 import styled, { css } from 'styled-components'
-import { space, color } from 'styled-system'
+import { space, color, typography, layout } from 'styled-system'
+import type {
+  SpaceProps,
+  ColorProps,
+  TypographyProps,
+  LayoutProps,
+} from 'styled-system'
+
+export const cssAsProps = {
+  text: {
+    /* -------------------------------------------- */
+    // color, backgroundColor
+    // @see https://styled-system.com/api#color
+    color,
+    /* -------------------------------------------- */
+    // fontFamily, fontSize, fontWeight, lineHeight, letterSpacing, textAlign, and fontStyle.
+    // @see https://styled-system.com/api#typography
+    typography,
+  },
+  box: {
+    /* -------------------------------------------- */
+    // margin, padding
+    // @see https://styled-system.com/api#space
+    space,
+    /* -------------------------------------------- */
+    // width, height, display, minWidth, minHeight, maxWidth, maxHeight, size, verticalAlign, overflow, overflowX, and overflowY
+    // @see https://styled-system.com/api#layout
+    layout,
+    /* -------------------------------------------- */
+  },
+} as const
+
+export type SelectLabelCssAsProps = keyof typeof cssAsProps
 
 namespace provideCssProps {
-  export const cssAsProps = {
-    block: css`
-      // margin & padding
-      // @see https://styled-system.com/api#space
-      ${space}
-
-      // @see https://styled-system.com/api#color
-      ${color}
-    `,
-  } as const
-
-  export type SelectLabelCssAsProps = keyof typeof cssAsProps
-
   export const as = (kind: SelectLabelCssAsProps) => {
-    return cssAsProps[kind]
+    return css`
+      ${Object.values(cssAsProps[kind]).map(prop => prop)}
+    `
   }
 }
 
-export const Button = styled.button`
+namespace CssProps {
+  export type As<L extends SelectLabelCssAsProps> = L extends 'button'
+    ? SpaceProps & ColorProps & TypographyProps & LayoutProps
+    : never
+}
+
+/*
+
+export type ButtonProps = CssProps.As<'block'>
+
+export const Button = styled.button<ButtonProps>`
   ${provideCssProps.as('block')}
 `
+
+*/
