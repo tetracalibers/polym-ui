a556db73df2a591e18e04ec3f9a01d869e4c2031
Kanno Ami (tomixy) <tomix05may@gmail.com>
2022-08-20 02:40:48 +0900

    [Button] transitionプロパティのargTypeを明示

--- packages/clickableTrigger/src/button/Button.stories.tsx
+++ packages/clickableTrigger/src/button/Button.stories.tsx
@@ -20,6 +20,9 @@ export default {
     direction: {
       ...controlTypeSelectAs('direction'),
     },
+    transition: {
+      ...controlType('text'),
+    },
     outlineWidth: {
       ...controlType('text'),
     },
