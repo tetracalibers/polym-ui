99260be6794692afd016b212b10f0b147ff28141
Kanno Ami (tomixy) <tomix05may@gmail.com>
2022-08-20 01:00:40 +0900

    [Button] Example17のstoryを一部記述

--- packages/clickableTrigger/src/button/Button.stories.tsx
+++ packages/clickableTrigger/src/button/Button.stories.tsx
@@ -783,3 +783,55 @@ Example16.args = {
     outline: 'none',
   },
 }
+
+export const Example17 = Template.bind({})
+Example17.args = {
+  children: 'Example17',
+  disabled: false,
+  fontFamily: 'Aboreto',
+  alignItems: 'center',
+  appearance: 'none',
+  backgroundColor: '#fff',
+  borderRadius: '24px',
+  borderStyle: 'none',
+  boxShadow:
+    'rgba(0, 0, 0, .2) 0 3px 5px -1px,rgba(0, 0, 0, .14) 0 6px 10px 0,rgba(0, 0, 0, .12) 0 1px 18px 0',
+  boxSizing: 'border-box',
+  color: '#3c4043',
+  cursor: 'pointer',
+  display: 'inline-flex',
+  fill: 'currentcolor',
+  fontSize: '14px',
+  fontWeight: '500',
+  height: '48px',
+  justifyContent: 'center',
+  letterSpacing: '.25px',
+  lineHeight: 'normal',
+  maxWidth: '100%',
+  overflow: 'visible',
+  paddingY: '2px',
+  paddingX: '24px',
+  position: 'relative',
+  textAlign: 'center',
+  textTransform: 'none',
+  transition:
+    'box-shadow 280ms cubic-bezier(.4, 0, .2, 1),opacity 15ms linear 30ms,transform 270ms cubic-bezier(0, 0, .2, 1) 0ms',
+  userSelect: 'none',
+  touchAction: 'manipulation',
+  width: 'auto',
+  willChange: 'transform,opacity',
+  zIndex: '0',
+  hoverStyle: {
+    background: '#F6F9FE',
+    color: '#174ea6',
+  },
+  activeStyle: {
+    boxShadow:
+      '0 4px 4px 0 rgb(60 64 67 / 30%), 0 8px 12px 6px rgb(60 64 67 / 15%)',
+    outline: 'none',
+  },
+  focusStyle: {
+    outline: 'none',
+    border: '2px solid #4285f4',
+  },
+}
