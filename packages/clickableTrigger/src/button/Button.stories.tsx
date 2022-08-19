e7a322bebbe56410e110a3d215e0e405ce156a7b
Kanno Ami (tomixy) <tomix05may@gmail.com>
2022-08-20 07:21:14 +0900

    [Button] Example29のstoryを記述

--- packages/clickableTrigger/src/button/Button.stories.tsx
+++ packages/clickableTrigger/src/button/Button.stories.tsx
@@ -1397,3 +1397,51 @@ Example28.args = {
     transform: 'translateY(0)',
   },
 }
+
+export const Example29 = Template.bind({})
+Example29.args = {
+  children: 'Example29',
+  disabled: false,
+  fontFamily: 'Aboreto',
+  alignItems: 'center',
+  appearance: 'none',
+  backgroundImage:
+    'radial-gradient(100% 100% at 100% 0, #5adaff 0, #5468ff 100%)',
+  border: '0',
+  borderRadius: '6px',
+  boxShadow:
+    'rgba(45, 35, 66, .4) 0 2px 4px,rgba(45, 35, 66, .3) 0 7px 13px -3px,rgba(58, 65, 111, .5) 0 -3px 0 inset',
+  boxSizing: 'border-box',
+  color: '#fff',
+  cursor: 'pointer',
+  display: 'inline-flex',
+  height: '48px',
+  justifyContent: 'center',
+  lineHeight: '1',
+  listStyle: 'none',
+  overflow: 'hidden',
+  paddingLeft: '16px',
+  paddingRight: '16px',
+  position: 'relative',
+  textAlign: 'left',
+  textDecoration: 'none',
+  transition: 'box-shadow .15s,transform .15s',
+  userSelect: 'none',
+  touchAction: 'manipulation',
+  whiteSpace: 'nowrap',
+  willChange: 'box-shadow,transform',
+  fontSize: '18px',
+  focusStyle: {
+    boxShadow:
+      '#3c4fe0 0 0 0 1.5px inset, rgba(45, 35, 66, .4) 0 2px 4px, rgba(45, 35, 66, .3) 0 7px 13px -3px, #3c4fe0 0 -3px 0 inset',
+  },
+  hoverStyle: {
+    boxShadow:
+      'rgba(45, 35, 66, .4) 0 4px 8px, rgba(45, 35, 66, .3) 0 7px 13px -3px, #3c4fe0 0 -3px 0 inset',
+    transform: 'translateY(-2px)',
+  },
+  activeStyle: {
+    boxShadow: '#3c4fe0 0 3px 7px inset',
+    transform: 'translateY(2px)',
+  },
+}
