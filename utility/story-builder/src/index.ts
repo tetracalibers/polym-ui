1442e422460aa70bc49b7c1479f4afc965bcf1c6
Kanno Ami (tomixy) <tomix05may@gmail.com>
2022-08-19 18:32:18 +0900

    [story-builder] select形式のCssPropsのoptions値をmdn-dataから取得し提供するモジュール作成

--- utility/story-builder/src/index.ts
+++ utility/story-builder/src/index.ts
@@ -1,1 +1,29 @@
+import { dumpJson, fromJson, logJson } from 'json-microscope'
 import * as mdnCssPropertiesJson from 'mdn-data/css/properties.json'
+import _ from 'lodash'
+
+const mdnCssProperties = fromJson(mdnCssPropertiesJson).default
+
+const acronymOf = (word: string) => word.slice(0, 1)
+
+const withoutVendorProperties = _.pick(
+  mdnCssProperties,
+  Object.keys(mdnCssProperties).filter((propName: string) => {
+    return acronymOf(propName) !== '-'
+  })
+)
+
+const pickSyntax = _.mapValues(withoutVendorProperties, val => {
+  return {
+    control: {
+      type: 'select',
+    },
+    options: _.words(val.syntax),
+  }
+})
+
+const convertedToCamelCase = _.mapKeys(pickSyntax, (_val, key) =>
+  _.camelCase(key)
+)
+
+export const getCssPropSelectType = convertedToCamelCase
