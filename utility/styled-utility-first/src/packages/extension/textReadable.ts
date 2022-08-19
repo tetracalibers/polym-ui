3cea61d9a2d12008502f4064fbb772185ed6306f
Kanno Ami (tomixy) <tomix05may@gmail.com>
2022-08-19 23:26:15 +0900

    [styled-utility-first] textTransformプロパティに対応

--- utility/styled-utility-first/src/packages/extension/textReadable.ts
+++ utility/styled-utility-first/src/packages/extension/textReadable.ts
@@ -5,6 +5,7 @@ import { mixinBuilder } from './helper/mixinMaker'
 const conf = {
   whiteSpace: Required<CSST.Property.WhiteSpace>(),
   wordWrap: Required<CSST.Property.WordWrap>(),
+  textTransform: Required<CSST.Property.TextTransform>(),
 }
 
 type Conf = typeof conf
