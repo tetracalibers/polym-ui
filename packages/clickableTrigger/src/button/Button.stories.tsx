bab42f941a54877770b4a8f11537b79c1557dcd8
Kanno Ami (tomixy) <tomix05may@gmail.com>
2022-08-19 23:47:38 +0900

    [Button] backgroundのcontrol.typeをtextに変更

--- packages/clickableTrigger/src/button/Button.stories.tsx
+++ packages/clickableTrigger/src/button/Button.stories.tsx
@@ -14,6 +14,9 @@ export default {
     flexDirection: {
       ...controlTypeSelectAs('flexDirection'),
     },
+    background: {
+      ...controlType('text'),
+    },
     backgroundColor: {
       ...controlType('color'),
     },
