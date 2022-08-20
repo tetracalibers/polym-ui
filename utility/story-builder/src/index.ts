6e32323335739e0d9e1e1891b4470d4044e49eaa
Kanno Ami (tomixy) <tomix05may@gmail.com>
2022-08-20 17:48:02 +0900

    [story-builder] cssStoryMetaデータを提供

--- utility/story-builder/src/index.ts
+++ utility/story-builder/src/index.ts
@@ -1,2 +1,5 @@
 // テスト用（バンドル時はコメントアウト）
 //import './dumpster/use-fs'
+
+import * as cssStoryMetaJson from './data/css-prop-doc.json'
+export const cssStoryMeta = JSON.parse(JSON.stringify(cssStoryMetaJson))
