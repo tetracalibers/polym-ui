eb7783dec84e1474eec8c13e99327dbdd481bd25
Kanno Ami (tomixy) <tomix05may@gmail.com>
2022-08-21 07:00:15 +0900

    [RotatingButton] rorateTo = frontの時のスタイル定義

--- packages/clickableTrigger/src/button/RotatingButton/style/RotatingStyled.ts
+++ packages/clickableTrigger/src/button/RotatingButton/style/RotatingStyled.ts
@@ -1,11 +1,12 @@
 import styled from 'styled-components'
 import { RotatingButtonProps } from './../model/props'
-import { provideCssProps, ResetCss } from 'styled-utility-first'
+import { ResetCss } from 'styled-utility-first'
+import { buttonSubStyle } from '../../substyle/mixin'
 
 export const RotatingStyled = styled.button<RotatingButtonProps>`
   ${ResetCss.button}
-  ${provideCssProps.as('componentBaseButton')}
-  
+  ${buttonSubStyle}
+
   /* 背景の基点 */
   position: relative;
   /* ボタンの形状 */
@@ -27,5 +28,31 @@ export const RotatingStyled = styled.button<RotatingButtonProps>`
     transform-style: preserve-3d;
     /* 数字が小さくなるほど早く回転 */
     transition: 0.5s;
+  }
+
+  /* 回転前 */
+  & span:nth-child(1) {
+    background: #fff;
+    color: #000;
+    /* 初めは回転なし */
+    transform: rotateX(0deg);
+    /* 回転する起点 */
+    transform-origin: 0 50% -25px;
+  }
+
+  &:hover span:nth-child(1) {
+    transform: rotateX(-90deg); /* X軸に-90度回転 */
+  }
+
+  /* 回転後 */
+  & span:nth-child(2) {
+    background: #000;
+    color: #fff;
+    transform: rotateX(90deg); /*はじめはX軸に90度回転*/
+    transform-origin: 0 50% -25px; /* 回転する起点 */
+  }
+
+  &:hover span:nth-child(2) {
+    transform: rotateX(0deg); /* X軸に0度回転 */
   }
 `
