c757cddf7d59068a482c322b565bf5bdd7dea0a6
Kanno Ami (tomixy) <tomix05may@gmail.com>
2022-08-21 09:55:37 +0900

    [RippleButton] コンポーネントを作成

---
+++ packages/clickableTrigger/src/button/RippleButton/components/RippleButton.tsx
@@ -0,0 +1,22 @@
+import { ComponentPropsWithoutRef, FC, ReactNode } from 'react'
+import { RippleButtonProps, _defaultProps } from '../model/props'
+import { TriggerClickModeStyled } from '../styled/TriggerAsClick'
+
+type ThisProps = ComponentPropsWithoutRef<'button'> & {
+  children: ReactNode
+} & RippleButtonProps
+
+const defaultProps = {
+  ..._defaultProps,
+  children: '',
+}
+
+const RippleButton: FC<ThisProps> = (
+  { ...props }: ThisProps = { ...defaultProps }
+) => {
+  return (
+    <TriggerClickModeStyled {...props}>{props.children}</TriggerClickModeStyled>
+  )
+}
+
+export default RippleButton
