ee318e991b916883ad4ec31bac64909a5f893ca5
Kanno Ami (tomixy) <tomix05may@gmail.com>
2022-08-21 10:27:03 +0900

    [RippleCircle] Reactコンポーネント作成

---
+++ packages/clickableTrigger/src/button/RippleCircle/components/RippleCircle.tsx
@@ -0,0 +1,23 @@
+import { ComponentPropsWithoutRef, FC, ReactNode } from 'react'
+import {
+  RippleCircleProps,
+  defaultProps as _defaultProps,
+} from '../model/props'
+import { RippleStyled } from '../styled/RippleCircle'
+
+type ThisProps = ComponentPropsWithoutRef<'button'> & {
+  children: ReactNode
+} & RippleCircleProps
+
+const defaultProps = {
+  ..._defaultProps,
+  children: '',
+}
+
+const RippleCircle: FC<ThisProps> = (
+  { ...props }: ThisProps = { ...defaultProps }
+) => {
+  return <RippleStyled {...props}>{props.children}</RippleStyled>
+}
+
+export default RippleCircle
