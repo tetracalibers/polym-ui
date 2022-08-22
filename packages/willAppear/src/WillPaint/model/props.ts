c3345a6c99d0c3412e7ea830d563f468da75462d
Kanno Ami (tomixy) <tomix05may@gmail.com>
2022-08-22 18:07:24 +0900

    [WillPaint] props定義

--- packages/willAppear/src/WillPaint/model/props.ts
+++ packages/willAppear/src/WillPaint/model/props.ts
@@ -2,7 +2,12 @@ import { getDefaultProps, getPropType, Required } from 'react-tsx-props'
 import { commonDefaultProps, CommonProps } from '../../common/props'
 import { styleDefaultProps, StyleProps } from '../css-props/props'
 
-const conf = {}
+export const extendFromOptions = ['left', 'right', 'top', 'bottom'] as const
+export type ExtendFromOptions = typeof extendFromOptions[number]
+
+const conf = {
+  extendFrom: Required<ExtendFromOptions>('left'),
+}
 type Conf = typeof conf
 
 export type CharacterProps = getPropType<Conf> & CommonProps & StyleProps
