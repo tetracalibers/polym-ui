30b052be1eab3600da975e2044341f214c32cd37
Kanno Ami (tomixy) <tomix05may@gmail.com>
2022-08-22 15:29:21 +0900

    [WillBorder] define styled-component

--- packages/willAppear/src/WillBorder/styled/index.ts
+++ packages/willAppear/src/WillBorder/styled/index.ts
@@ -1,22 +1,75 @@
-import styled, { css, StyledComponent } from 'styled-components'
+import styled, { StyledComponent } from 'styled-components'
 import { CharacterProps } from '../model/props'
 import * as $ from './keyframes'
 import { TagType } from '../../common/props'
 
-const animation = {
-  fadeFrom: {
-    here: $.fadeInKeyframes,
-    bottom: $.fadeUpKeyframes,
-    top: $.fadeDownKeyframes,
-    left: $.fadeLeftKeyframes,
-    right: $.fadeRightKeyframes,
-  },
-}
-
 // 上下線が伸びる設定
-export const WillHorizontalLine = styled.div``
+export const WillHorizontalLine: StyledComponent<
+  TagType,
+  {}
+> = styled.div<CharacterProps>`
+  position: relative; /* 枠線が書かれる基点*/
+
+  /*上下線*/
+  &::before,
+  &::after {
+    position: absolute;
+    content: '';
+    width: 0;
+    height: 1px;
+    background: #333; /* 枠線の色*/
+  }
+
+  /*上線*/
+  &::before {
+    top: 0;
+    left: 0;
+    animation: ${$.horizontalLineKeyframes} 0.5s linear 0s forwards; /*表示されて0秒後に上線が0.5秒かけて表示*/
+  }
+
+  /*下線*/
+  &::after {
+    bottom: 0;
+    right: 0;
+    animation: ${$.horizontalLineKeyframes} 0.5s linear 1s forwards; /*表示されて1秒後に下線が0.5秒かけて表示*/
+  }
+`
 
 // 左右線が伸びる設定
-export const WillVerticalLine = styled.div``
+export const WillVerticalLine: StyledComponent<
+  TagType,
+  {}
+> = styled.div<CharacterProps>`
+  /*左右線*/
+  &::before,
+  &::after {
+    position: absolute;
+    content: '';
+    width: 1px;
+    height: 0;
+    background: #333; /* 枠線の色*/
+  }
 
-export const ChildrenWrapper: StyledComponent<TagType, {}> = styled.div``
+  /*右線*/
+  &::before {
+    top: 0;
+    right: 0;
+    animation: ${$.verticalLineKeyframes} 0.5s linear 0.5s forwards; /*表示されて0.5秒後に右線が0.5秒かけて表示*/
+  }
+
+  /*左線*/
+  &::after {
+    bottom: 0;
+    left: 0;
+    animation: ${$.verticalLineKeyframes} 0.5s linear 1.5s forwards; /*表示されて1.5秒後に左線が0.5秒かけて表示*/
+  }
+`
+
+export const ChildrenWrapper: StyledComponent<
+  TagType,
+  {}
+> = styled.div<CharacterProps>`
+  /*枠線内側の要素*/
+  animation: ${$.childrenDelayKeyframes} 0.5s linear 1.5s forwards; /*1.5秒後に中央のエリアが0.5秒かけて表示*/
+  opacity: 0; /*初期値を透過0にする*/
+`
--- packages/willAppear/src/WillBorder/styled/keyframes.ts
+++ packages/willAppear/src/WillBorder/styled/keyframes.ts
@@ -1,54 +1,28 @@
 import { keyframes } from 'styled-components'
 
-export const fadeInKeyframes = keyframes`
-  from {
-    opacity: 0;
+export const horizontalLineKeyframes = keyframes`
+  0% {
+    width: 0%;
   }
-  to {
-    opacity: 1;
-  }
-`
-
-export const fadeUpKeyframes = keyframes`
-  from {
-    opacity: 0;
-    transform: translateY(100px);
-  }
-  to {
-    opacity: 1;
-    transform: translateY(0);
+  100% {
+    width: 100%;
   }
 `
 
-export const fadeDownKeyframes = keyframes`
-  from {
-    opacity: 0;
-    transform: translateY(-100px);
+export const verticalLineKeyframes = keyframes`
+  0% {
+    height: 0%;
   }
-  to {
-    opacity: 1;
-    transform: translateY(0);
+  100% {
+    height: 100%;
   }
 `
 
-export const fadeLeftKeyframes = keyframes`
-  from {
+export const childrenDelayKeyframes = keyframes`
+  0% {
     opacity: 0;
-    transform: translateX(-100px);
   }
-  to {
+  100% {
     opacity: 1;
-    transform: translateX(0);
-  }
-`
-
-export const fadeRightKeyframes = keyframes`
-  from {
-    opacity: 0;
-    transform: translateX(100px);
-  }
-  to {
-    opacity: 1;
-    transform: translateX(0);
   }
 `
