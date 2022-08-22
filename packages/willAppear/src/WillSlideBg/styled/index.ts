614cc9247e3410484168ddfb49aea10eb3f5138c
Kanno Ami (tomixy) <tomix05may@gmail.com>
2022-08-22 18:44:27 +0900

    [WillSlideBg] slideFrom propsに応じたCSSを定義

--- packages/willAppear/src/WillSlideBg/styled/index.ts
+++ packages/willAppear/src/WillSlideBg/styled/index.ts
@@ -1,7 +1,16 @@
-import styled, { css, StyledComponent } from 'styled-components'
+import styled, { StyledComponent } from 'styled-components'
 import { CharacterProps } from '../model/props'
 import * as $ from './keyframes'
 import { TagType } from '../../common/props'
+
+const slideAnimation = {
+  from: {
+    left: $.bgSlideFromLeft,
+    right: $.bgSlideFromRight,
+    top: $.bgSlideFromTop,
+    bottom: $.bgSlideFromBottom,
+  },
+}
 
 export const SlideBackground: StyledComponent<
   TagType,
@@ -13,6 +22,18 @@ export const SlideBackground: StyledComponent<
   position: relative;
   overflow: hidden; /*はみ出た色要素を隠す*/
   opacity: 0;
+
+  /* スライドする背景 */
+  &::before {
+    animation-name: ${({ slideFrom }) => slideAnimation.from[slideFrom]};
+    animation-duration: 1s;
+    animation-fill-mode: forwards;
+    content: '';
+    position: absolute;
+    width: 100%;
+    height: 100%;
+    background-color: #666; /*伸びる背景色の設定*/
+  }
 `
 
 export const ChildrenWrapper: StyledComponent<
--- packages/willAppear/src/WillSlideBg/styled/keyframes.ts
+++ packages/willAppear/src/WillSlideBg/styled/keyframes.ts
@@ -17,3 +17,75 @@ export const childrenDelayKeyframes = keyframes`
     opacity: 1;
   }
 `
+
+export const bgSlideFromLeft = keyframes`
+  0% {
+    transform-origin: left;
+    transform: scaleX(0);
+  }
+  50% {
+    transform-origin: left;
+    transform: scaleX(1);
+  }
+  50.001% {
+    transform-origin: right;
+  }
+  100% {
+    transform-origin: right;
+    transform: scaleX(0);
+  }
+`
+
+export const bgSlideFromRight = keyframes`
+  0% {
+    transform-origin: right;
+    transform: scaleX(0);
+  }
+  50% {
+    transform-origin: right;
+    transform: scaleX(1);
+  }
+  50.001% {
+    transform-origin: left;
+  }
+  100% {
+    transform-origin: left;
+    transform: scaleX(0);
+  }
+`
+
+export const bgSlideFromBottom = keyframes`
+  0% {
+    transform-origin: bottom;
+    transform: scaleY(0);
+  }
+  50% {
+    transform-origin: bottom;
+    transform: scaleY(1);
+  }
+  50.001% {
+    transform-origin: top;
+  }
+  100% {
+    transform-origin: top;
+    transform: scaleY(0);
+  }
+`
+
+export const bgSlideFromTop = keyframes`
+  0% {
+    transform-origin: top;
+    transform: scaleY(0);
+  }
+  50% {
+    transform-origin: top;
+    transform: scaleY(1);
+  }
+  50.001% {
+    transform-origin: bottom;
+  }
+  100% {
+    transform-origin: bottom;
+    transform: scaleY(0);
+  }
+`
