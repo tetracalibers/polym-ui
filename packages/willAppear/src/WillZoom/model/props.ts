623a4274f93256d6db6faeb1764285dc9e4dd0ad
Kanno Ami (tomixy) <tomix05may@gmail.com>
2022-08-22 13:35:38 +0900

    [willZoom] props定義

--- packages/willAppear/src/WillZoom/model/props.ts
+++ packages/willAppear/src/WillZoom/model/props.ts
@@ -2,7 +2,12 @@ import { getDefaultProps, getPropType, Required } from 'react-tsx-props'
 import { commonDefaultProps, CommonProps } from '../../common/props'
 import { styleDefaultProps, StyleProps } from '../css-props/props'
 
-const conf = {}
+export const zoomOptions = ['in', 'out'] as const
+export type ZoomOptions = typeof zoomOptions[number]
+
+const conf = {
+  zoom: Required<ZoomOptions>('in'),
+}
 type Conf = typeof conf
 
 export type CharacterProps = getPropType<Conf> & CommonProps & StyleProps
