64f2d3d2b8a3025343af341fffe903d56e407089
Kanno Ami (tomixy) <tomix05may@gmail.com>
2022-08-22 12:54:40 +0900

    [willFlip] スタイル定義

--- packages/willAppear/src/WillFlip/styled/index.ts
+++ packages/willAppear/src/WillFlip/styled/index.ts
@@ -1,19 +1,37 @@
-import styled, { css, StyledComponent } from 'styled-components'
+import styled, { css, Keyframes, StyledComponent } from 'styled-components'
 import { CharacterProps } from '../model/props'
 import * as $ from './keyframes'
 import { TagType } from '../../common/props'
+import _ from 'lodash'
+import { match } from 'ts-pattern'
 
 const animation = {
-  fadeFrom: {
-    here: $.fadeInKeyframes,
-    bottom: $.fadeUpKeyframes,
-    top: $.fadeDownKeyframes,
-    left: $.fadeLeftKeyframes,
-    right: $.fadeRightKeyframes,
-  },
+  flipTo: {
+    bottom: $.flipDownKeyframes,
+    left: $.flipLeftKeyframes,
+    right: $.flipRightKeyframes,
+    leftTop: $.flipLeftTopKeyframes,
+    rightTop: $.flipRightTopKeyframes,
+  } as Record<string, Keyframes>,
 }
 
-const thisCss = css<CharacterProps>``
+const perspectiveOrigin = (flipTo: CharacterProps['flipTo']) => {
+  return match(flipTo)
+    .with('left', 'right', () => {
+      return css`
+        perspective-origin: ${flipTo} center;
+      `
+    })
+    .otherwise(() => '')
+}
+
+const thisCss = css<CharacterProps>`
+  animation-name: ${({ flipTo }) => animation.flipTo[_.camelCase(flipTo)]};
+  animation-duration: ${({ animationDuration }) => animationDuration};
+  animation-fill-mode: forwards;
+  ${({ flipTo }) => perspectiveOrigin(flipTo)}
+  opacity: 0;
+`
 
 export const StyledElement: StyledComponent<TagType, {}> = styled.div`
   ${thisCss}
--- packages/willAppear/src/WillFlip/styled/keyframes.ts
+++ packages/willAppear/src/WillFlip/styled/keyframes.ts
@@ -1,54 +1,56 @@
 import { keyframes } from 'styled-components'
 
-export const fadeInKeyframes = keyframes`
+export const flipDownKeyframes = keyframes`
   from {
+    transform: perspective(2500px) rotateX(100deg);
     opacity: 0;
   }
   to {
+    transform: perspective(2500px) rotateX(0);
     opacity: 1;
   }
 `
 
-export const fadeUpKeyframes = keyframes`
+export const flipLeftKeyframes = keyframes`
   from {
+    transform: perspective(600px) translate3d(0, 0, 0) rotateY(30deg);
     opacity: 0;
-    transform: translateY(100px);
   }
   to {
+    transform: perspective(600px) translate3d(0, 0, 0) rotateY(0);
     opacity: 1;
-    transform: translateY(0);
   }
 `
 
-export const fadeDownKeyframes = keyframes`
+export const flipLeftTopKeyframes = keyframes`
   from {
+    transform: translate(-20px, 80px) rotate(-15deg);
     opacity: 0;
-    transform: translateY(-100px);
   }
   to {
+    transform: translate(0, 0) rotate(0);
     opacity: 1;
-    transform: translateY(0);
   }
 `
 
-export const fadeLeftKeyframes = keyframes`
+export const flipRightKeyframes = keyframes`
   from {
+    transform: perspective(600px) translate3d(0, 0, 0) rotateY(-30deg);
     opacity: 0;
-    transform: translateX(-100px);
   }
   to {
+    transform: perspective(600px) translate3d(0, 0, 0) rotateY(0);
     opacity: 1;
-    transform: translateX(0);
   }
 `
 
-export const fadeRightKeyframes = keyframes`
+export const flipRightTopKeyframes = keyframes`
   from {
+    transform: translate(-20px, 80px) rotate(25deg);
     opacity: 0;
-    transform: translateX(100px);
   }
   to {
+    transform: translate(0, 1) rotate(0);
     opacity: 1;
-    transform: translateX(0);
   }
 `
