a4e184d203c291ed4896a3250a7b31a890a44711
Kanno Ami (tomixy) <tomix05may@gmail.com>
2022-08-22 12:03:31 +0900

    [willflip] props定義

--- packages/willAppear/src/WillFlip/model/props.ts
+++ packages/willAppear/src/WillFlip/model/props.ts
@@ -2,7 +2,18 @@ import { getDefaultProps, getPropType, Required } from 'react-tsx-props'
 import { commonDefaultProps, CommonProps } from '../../common/props'
 import { styleDefaultProps, StyleProps } from '../css-props/props'
 
-const conf = {}
+export const flipToOptions = [
+  'bottom',
+  'left',
+  'right',
+  'left-top',
+  'right-top',
+] as const
+export type FlipToOptions = typeof flipToOptions[number]
+
+const conf = {
+  flipTo: Required<FlipToOptions>('bottom'),
+}
 type Conf = typeof conf
 
 export type CharacterProps = getPropType<Conf> & CommonProps & StyleProps
