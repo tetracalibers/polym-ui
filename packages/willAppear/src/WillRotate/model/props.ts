d2a863f02f037e95294c0f2aee018344b50ea5d5
Kanno Ami (tomixy) <tomix05may@gmail.com>
2022-08-22 13:02:56 +0900

    [willRotate] props定義

--- packages/willAppear/src/WillRotate/model/props.ts
+++ packages/willAppear/src/WillRotate/model/props.ts
@@ -2,7 +2,17 @@ import { getDefaultProps, getPropType, Required } from 'react-tsx-props'
 import { commonDefaultProps, CommonProps } from '../../common/props'
 import { styleDefaultProps, StyleProps } from '../css-props/props'
 
-const conf = {}
+export const rotateToOptions = [
+  'vertical',
+  'horizontal',
+  'left',
+  'right',
+] as const
+export type RotateToOptions = typeof rotateToOptions[number]
+
+const conf = {
+  rotateTo: Required<RotateToOptions>('vertical'),
+}
 type Conf = typeof conf
 
 export type CharacterProps = getPropType<Conf> & CommonProps & StyleProps
