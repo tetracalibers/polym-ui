d9501750750959a1d294571bb84b4e9dc767204e
Kanno Ami (tomixy) <tomix05may@gmail.com>
2022-08-22 09:39:09 +0900

    [WillFade] アニメーションスタイルとcssProps定義

--- packages/willAppear/src/WillFade/css-props/props.ts
+++ packages/willAppear/src/WillFade/css-props/props.ts
@@ -1,7 +1,9 @@
 import { getDefaultProps, getPropType, Required } from 'react-tsx-props'
 import * as CSST from 'csstype'
 
-const conf = {}
+const conf = {
+  animationDuration: Required<CSST.Property.AnimationDuration>('0.5s'),
+}
 type Conf = typeof conf
 
 export type StyleProps = getPropType<Conf>
--- packages/willAppear/src/WillFade/styled/index.ts
+++ packages/willAppear/src/WillFade/styled/index.ts
@@ -1,9 +1,25 @@
-import styled, { css } from 'styled-components'
+import styled, { css, keyframes } from 'styled-components'
 import { ResetCss } from 'styled-utility-first'
 import { CharacterProps } from '../model/props'
 import { match } from 'ts-pattern'
+import * as $ from './keyframes'
 
-const thisCss = css<CharacterProps>``
+const animation = {
+  fadeFrom: {
+    here: $.fadeInKeyframes,
+    bottom: $.fadeUpKeyframes,
+    top: $.fadeDownKeyframes,
+    left: $.fadeLeftKeyframes,
+    right: $.fadeRightKeyframes,
+  },
+}
+
+const thisCss = css<CharacterProps>`
+  animation-name: ${({ fadeFrom }) => animation.fadeFrom[fadeFrom]};
+  animation-duration: ${({ animationDuration }) => animationDuration};
+  animation-fill-mode: forwards;
+  opacity: 0;
+`
 
 export const StyledButton = styled.button<CharacterProps>`
   ${ResetCss.button}
