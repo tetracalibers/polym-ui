2e462260f38a0886581b98651bd7e3b33753aee3
Kanno Ami (tomixy) <tomix05may@gmail.com>
2022-08-23 15:12:40 +0900

    [BlowingTag] tailPos = topの場合のスタイルを追加

--- packages/shapeContainer/src/BlowingTag/styled/index.ts
+++ packages/shapeContainer/src/BlowingTag/styled/index.ts
@@ -3,6 +3,57 @@ import { CharacterProps } from '../model/props'
 import { ElementType } from 'react'
 import { match } from 'ts-pattern'
 
+const afterPosition = (tailPos: CharacterProps['tailPos']) => {
+  return match(tailPos)
+    .with('bottom', () => {
+      return css`
+        bottom: 0;
+        left: 50%;
+      `
+    })
+    .with('top', () => {
+      return css`
+        top: 0;
+        left: 50%;
+      `
+    })
+    .otherwise(() => '')
+}
+
+const afterVisibleBorder = (tailPos: CharacterProps['tailPos']) => {
+  return match(tailPos)
+    .with('bottom', () => {
+      return css`
+        border-top-color: #333;
+        border-bottom: 0;
+      `
+    })
+    .with('top', () => {
+      return css`
+        border-bottom-color: #333;
+        border-top: 0;
+      `
+    })
+    .otherwise(() => '')
+}
+
+const afterMargin = (tailPos: CharacterProps['tailPos']) => {
+  return match(tailPos)
+    .with('bottom', () => {
+      return css`
+        margin-left: calc(50px * 0.13 * -1);
+        margin-bottom: calc(50px * 0.13 * -1);
+      `
+    })
+    .with('top', () => {
+      return css`
+        margin-left: calc(50px * 0.13 * -1);
+        margin-top: calc(50px * 0.13 * -1);
+      `
+    })
+    .otherwise(() => '')
+}
+
 const thisCss = css<CharacterProps>`
   position: relative;
   width: 50px;
@@ -13,15 +64,12 @@ const thisCss = css<CharacterProps>`
   &::after {
     content: '';
     position: absolute;
-    bottom: 0;
-    left: 50%;
+    ${({ tailPos }) => afterPosition(tailPos)}
     width: 0;
     height: 0;
     border: calc(50px * 0.13) solid transparent;
-    border-top-color: #333;
-    border-bottom: 0;
-    margin-left: calc(50px * 0.13 * -1);
-    margin-bottom: calc(50px * 0.13 * -1);
+    ${({ tailPos }) => afterVisibleBorder(tailPos)}
+    ${({ tailPos }) => afterMargin(tailPos)}
   }
 `
 
