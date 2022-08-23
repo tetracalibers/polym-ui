9878d23bbff0359aff86ab7b33a8e5c6a2b114ef
Kanno Ami (tomixy) <tomix05may@gmail.com>
2022-08-23 12:23:21 +0900

    [willAppear -> common] as propsの情報修正

--- packages/willAppear/src/common/props.ts
+++ packages/willAppear/src/common/props.ts
@@ -1,19 +1,9 @@
-import { ComponentType } from 'react'
 import { getDefaultProps, getPropType } from 'react-tsx-props'
-import { IntrinsicElementsKeys, StyledComponent } from 'styled-components'
-
-export type TagType = StyledComponent<
-  IntrinsicElementsKeys | ComponentType<any>,
-  {}
->
 
 const conf = {}
 type Conf = typeof conf
 
-export type CommonProps = getPropType<Conf> & {
-  as: TagType
-}
+export type CommonProps = getPropType<Conf>
 export const commonDefaultProps = {
   ...getDefaultProps<CommonProps>(conf),
-  as: 'div' as TagType,
 }
