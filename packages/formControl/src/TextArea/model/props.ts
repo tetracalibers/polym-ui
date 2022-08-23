dd3414b6c8cf15d461ff9530addfb785c4cc7397
Kanno Ami (tomixy) <tomix05may@gmail.com>
2022-08-24 08:44:16 +0900

    [TextArea] props定義

--- packages/formControl/src/TextArea/model/props.ts
+++ packages/formControl/src/TextArea/model/props.ts
@@ -2,7 +2,11 @@ import { getDefaultProps, getPropType, Required } from 'react-tsx-props'
 import { commonDefaultProps, CommonProps } from '../../common/props'
 import { styleDefaultProps, StyleProps } from '../css-props/props'
 
-const conf = {}
+const conf = {
+  minRows: Required<number>(5),
+  maxRows: Required<number>(10),
+  hasError: Required<boolean>(false),
+}
 type Conf = typeof conf
 
 export type CharacterProps = getPropType<Conf> & CommonProps & StyleProps
--- packages/willAppear/src/WillBlur/css-props/props.ts
+++ packages/willAppear/src/WillBlur/css-props/props.ts
@@ -1,19 +1,8 @@
 import { getDefaultProps, getPropType, Required } from 'react-tsx-props'
-
-type CreateArrayWithLengthX<
-  LENGTH extends number,
-  ACC extends unknown[] = []
-> = ACC['length'] extends LENGTH
-  ? ACC
-  : CreateArrayWithLengthX<LENGTH, [...ACC, 1]>
-
-type NumericRange<
-  START_ARR extends number[],
-  END extends number,
-  ACC extends number = never
-> = START_ARR['length'] extends END
-  ? ACC | END
-  : NumericRange<[...START_ARR, 1], END, ACC | START_ARR['length']>
+import {
+  CreateArrayWithLengthX,
+  NumericRange,
+} from '../../common/utility/NumericRange'
 
 export type BlurRadiusRange = NumericRange<CreateArrayWithLengthX<1>, 20>
 export const blurRadiusRange = [...new Array(20)].map((_, idx) => idx + 1)
