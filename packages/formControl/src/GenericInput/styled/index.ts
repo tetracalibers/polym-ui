90f62f4e4ad08290b0ef87f5558455c944f0d856
Kanno Ami (tomixy) <tomix05may@gmail.com>
2022-08-23 21:29:53 +0900

    [GenericInput] デザイン調整

--- packages/formControl/src/GenericInput/styled/index.ts
+++ packages/formControl/src/GenericInput/styled/index.ts
@@ -8,11 +8,24 @@ export const StyledElement = styled.input`
     appearance: none;
     width: 100%;
     max-width: 300px;
-    padding: 10px 20px 8px 20px;
+    padding: 11px 12px 12px 9px;
+    box-sizing: border-box;
+    outline: none;
+    height: 38px;
     border-radius: 4px;
     border: 1px solid #ccc;
-    box-shadow: 1px 1px 4px rgba(0, 0, 0, 0.1) inset;
     /* 16px以上を推奨 */
     font-size: 16px;
+    line-height: 19px;
+
+    &::-webkit-outer-spin-button,
+    &::-webkit-inner-spin-button {
+      -webkit-appearance: none;
+      margin: 0;
+    }
+
+    &[type='number'] {
+      -moz-appearance: textfield;
+    }
   }
 `
