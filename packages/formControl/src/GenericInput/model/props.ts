5892d8e31dc22364d2387380476fd173b9997181
Kanno Ami (tomixy) <tomix05may@gmail.com>
2022-08-23 21:22:41 +0900

    [GenericsInput] type属性を必須化

--- packages/formControl/src/GenericInput/model/props.ts
+++ packages/formControl/src/GenericInput/model/props.ts
@@ -2,7 +2,9 @@ import { getDefaultProps, getPropType, Required } from 'react-tsx-props'
 import { commonDefaultProps, CommonProps } from '../../common/props'
 import { styleDefaultProps, StyleProps } from '../css-props/props'
 
-const conf = {}
+const conf = {
+  type: Required<HTMLInputElement['type']>('text'),
+}
 type Conf = typeof conf
 
 export type CharacterProps = getPropType<Conf> & CommonProps & StyleProps
