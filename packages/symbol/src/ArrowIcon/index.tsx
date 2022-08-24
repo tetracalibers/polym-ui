058d6a66bbf2f5dea8e8e2496d4aee003beb3d7f
Kanno Ami (tomixy) <tomix05may@gmail.com>
2022-08-24 19:21:36 +0900

    [ArrowIcon] コンポーネント定義

--- packages/symbol/src/ArrowIcon/index.tsx
+++ packages/symbol/src/ArrowIcon/index.tsx
@@ -1,31 +1,25 @@
 import _ from 'lodash'
 import { ElementType, forwardRef, ReactElement } from 'react'
-import {
-  PolymorphicComponentPropWithRef,
-  PolymorphicRef,
-} from '../common/polymorphic/standard'
+import { SpanComponentPropWithRef } from '../common/polymorphic/fixedAs'
+import { PolymorphicRef } from '../common/polymorphic/standard'
 import { CharacterProps, defaultProps } from './model/props'
 import { StyledElement } from './styled'
 
-export type ArrowIconProps<As extends ElementType> =
-  PolymorphicComponentPropWithRef<As, CharacterProps>
+export type ArrowIconProps = Omit<
+  SpanComponentPropWithRef<CharacterProps>,
+  'children'
+>
 
-export type ArrowIconComponent = <As extends ElementType>(
-  props: ArrowIconProps<As>
-) => ReactElement | null
+export type ArrowIconComponent = (props: ArrowIconProps) => ReactElement | null
 
 export const ArrowIcon: ArrowIconComponent = forwardRef(
   <As extends ElementType>(
-    { as, children, ..._props }: ArrowIconProps<As>,
+    { ..._props }: ArrowIconProps,
     ref?: PolymorphicRef<As>
   ) => {
     const props = _.mergeWith(_props, defaultProps, (input, defaul) =>
       _.isUndefined(input) ? defaul : input
     )
-    return (
-      <StyledElement {...props} ref={ref} as={as as unknown as undefined}>
-        {children}
-      </StyledElement>
-    )
+    return <StyledElement {...props} ref={ref} />
   }
 )
