b6fc369a4d7ae86c0e29d51f321069f159b66ead
Kanno Ami (tomixy) <tomix05may@gmail.com>
2022-08-24 16:58:27 +0900

    [VerticalStack]スタイル定義

--- packages/layout/src/VerticalStack/styled/index.ts
+++ packages/layout/src/VerticalStack/styled/index.ts
@@ -2,6 +2,40 @@ import styled, { css } from 'styled-components'
 import { CharacterProps } from '../model/props'
 import { ElementType } from 'react'
 import { match } from 'ts-pattern'
-import { ColorPalette } from 'styled-utility-first'
+import { ColorPalette, Truthy } from 'styled-utility-first'
 
-export const StyledElement = styled.div``
+export const StyledElement = styled.div<CharacterProps>`
+  && {
+    display: flex;
+    flex-direction: column;
+    justify-content: flex-start;
+  }
+
+  & > * {
+    /* 既存の垂直marginを削除 */
+    margin-top: 0;
+    margin-bottom: 0;
+  }
+
+  ${({ recursive, space }) => {
+    return recursive
+      ? css`
+          & * + * {
+            margin-top: ${space};
+          }
+        `
+      : css`
+          & > * + * {
+            margin-top: ${space};
+          }
+        `
+  }}
+
+  ${({ separateFrom }) => {
+    return Truthy(separateFrom).when(css`
+      & > :nth-child(${separateFrom}) {
+        margin-bottom: auto;
+      }
+    `)
+  }}
+`
