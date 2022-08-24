a5ae0ac563fda7896d5ead9dd5607c61fecfaf53
Kanno Ami (tomixy) <tomix05may@gmail.com>
2022-08-24 17:34:25 +0900

    [HorizontalStack] スタイル定義

--- packages/layout/src/HorizontalStack/styled/index.ts
+++ packages/layout/src/HorizontalStack/styled/index.ts
@@ -4,4 +4,27 @@ import { ElementType } from 'react'
 import { match } from 'ts-pattern'
 import { ColorPalette } from 'styled-utility-first'
 
-export const StyledElement = styled.div``
+export const StyledElement = styled.div<CharacterProps>`
+  && {
+    display: flex;
+    flex-wrap: wrap;
+    gap: ${({ space }) => space};
+  }
+
+  && > * > * {
+    /* 子要素の伸長を許可 */
+    flex-grow: 1;
+    /* breakpointでレイアウトを切り替え */
+    flex-basis: calc((${({ breakpoint }) => breakpoint} - 100%) * 999);
+  }
+
+  /* limit以上の子要素が存在すれば縦並びに切り替え */
+  ${({ limit }) => {
+    return css`
+      && > :nth-last-child(n + ${limit}),
+      && > :nth-last-child(n + ${limit} ~ *) {
+        flex-basis: 100%;
+      }
+    `
+  }}
+`
