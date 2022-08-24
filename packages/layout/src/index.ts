37f691de47e58ab361285bfaaeca8410ef23bc9f
Kanno Ami (tomixy) <tomix05may@gmail.com>
2022-08-24 23:44:42 +0900

    [layout] 他のパッケージからimportできるようビルド

---
+++ packages/layout/src/index.ts
@@ -0,0 +1,13 @@
+import { VerticalCenter } from './VerticalCenter'
+import { HaveSidebar } from './HaveSidebar'
+import { HorizontalStack } from './HorizontalStack'
+import { VerticalStack } from './VerticalStack'
+import { HorizontalCenter } from './HorizontalCenter'
+
+export {
+  VerticalCenter,
+  HaveSidebar,
+  HorizontalStack,
+  HorizontalCenter,
+  VerticalStack,
+}
