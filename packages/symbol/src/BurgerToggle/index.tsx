56a31c50cb9b96b2dc3266376eab7a81958a1058
Kanno Ami (tomixy) <tomix05may@gmail.com>
2022-08-24 23:12:04 +0900

    [BurgerToggle] as指定可能な形に定義を修正

--- packages/symbol/src/BurgerToggle/index.tsx
+++ packages/symbol/src/BurgerToggle/index.tsx
@@ -1,27 +1,31 @@
 import _ from 'lodash'
 import { ElementType, forwardRef, ReactElement } from 'react'
-import { DivComponentPropWithRef } from '../common/polymorphic/fixedAs'
-import { PolymorphicRef } from '../common/polymorphic/standard'
+import {
+  PolymorphicComponentPropWithRef,
+  PolymorphicRef,
+} from '../common/polymorphic/standard'
 import { CharacterProps, defaultProps } from './model/props'
 import { StyledElement } from './styled'
 
-export type BurgerToggleProps = Omit<
-  DivComponentPropWithRef<CharacterProps>,
+export type BurgerToggleProps<As extends ElementType> = Omit<
+  PolymorphicComponentPropWithRef<As, CharacterProps>,
   'children'
 >
 
-export type BurgerToggleComponent = (
-  props: BurgerToggleProps
+export type BurgerToggleComponent = <As extends ElementType>(
+  props: BurgerToggleProps<As>
 ) => ReactElement | null
 
 export const BurgerToggle: BurgerToggleComponent = forwardRef(
   <As extends ElementType>(
-    { ..._props }: BurgerToggleProps,
+    { as, ..._props }: BurgerToggleProps<As>,
     ref?: PolymorphicRef<As>
   ) => {
     const props = _.mergeWith(_props, defaultProps, (input, defaul) =>
       _.isUndefined(input) ? defaul : input
     )
-    return <StyledElement {...props} ref={ref} />
+    return (
+      <StyledElement {...props} ref={ref} as={as as unknown as undefined} />
+    )
   }
 )
