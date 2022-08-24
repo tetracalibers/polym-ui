e65a5264a2ca88e7cde710967c72e6fd256aca70
Kanno Ami (tomixy) <tomix05may@gmail.com>
2022-08-24 11:51:20 +0900

    [TextArea] 最大行数まで改行した時に高さが増える仕組みを構築

--- packages/formControl/src/TextArea/index.tsx
+++ packages/formControl/src/TextArea/index.tsx
@@ -1,5 +1,12 @@
 import _ from 'lodash'
-import { ElementType, forwardRef, ReactElement } from 'react'
+import {
+  ChangeEvent,
+  ElementType,
+  forwardRef,
+  ReactElement,
+  useCallback,
+  useState,
+} from 'react'
 import { TextAreaComponentPropWithRef } from '../common/polymorphic/fixedAs'
 import { PolymorphicRef } from '../common/polymorphic/standard'
 import { CharacterProps, defaultProps } from './model/props'
@@ -17,8 +24,44 @@ export const TextArea: TextAreaComponent = forwardRef(
     const props = _.mergeWith(_props, defaultProps, (input, defaul) =>
       _.isUndefined(input) ? defaul : input
     )
+    const { onChange, minRows, maxRows, lineHeight } = props
+
+    const [textareaRows, setTextareaRows] = useState(Math.min(minRows, maxRows))
+
+    // 最大行数まで改行した時に高さが増える仕組み
+    // 入力が変わったときにe.target.scrollHeightから現在表示している行数を把握
+    // -> setTextareaで最大行数maxRowsを超えないように動的に変更
+    const changeHeight = useCallback(
+      (e: ChangeEvent<HTMLTextAreaElement>) => {
+        const previewsRows = e.target.rows
+        // 行数のリセット
+        e.target.rows = minRows
+        // 現在の行数
+        const currentRows = Math.floor(e.target.scrollHeight / lineHeight)
+        // 変化がなければ
+        if (currentRows === previewsRows) {
+          e.target.rows = currentRows
+        }
+        // 最大を超えたら
+        if (currentRows >= maxRows) {
+          e.target.rows = maxRows
+          e.target.scrollTop = e.target.scrollHeight
+        }
+        // 最大を超えないように行数をセット
+        setTextareaRows(Math.min(currentRows, maxRows))
+        onChange && onChange(e)
+      },
+      [onChange, minRows, maxRows]
+    )
+
     return (
-      <StyledElement {...props} ref={ref}>
+      <StyledElement
+        {...props}
+        ref={ref}
+        onChange={changeHeight}
+        aria-label={props.placeholder}
+        rows={textareaRows}
+      >
         {children}
       </StyledElement>
     )
