d0002714da94f56de9b46589d2d3ab402f5800a6
Kanno Ami (tomixy) <tomix05may@gmail.com>
2022-08-25 08:00:08 +0900

    [AspectFrame] スタイル定義

--- packages/layout/src/AspectFrame/styled/index.ts
+++ packages/layout/src/AspectFrame/styled/index.ts
@@ -1,7 +1,31 @@
-import styled, { css } from 'styled-components'
+import styled from 'styled-components'
 import { CharacterProps } from '../model/props'
-import { ElementType } from 'react'
-import { match } from 'ts-pattern'
-import { ColorPalette } from 'styled-utility-first'
 
-export const StyledElement = styled.div<CharacterProps>``
+export const StyledElement = styled.div<CharacterProps>`
+  --height: ${({ ratioY }) => ratioY};
+  --width: ${({ ratioX }) => ratioX};
+
+  && {
+    padding-bottom: calc(var(--height) / var(--width) * 100%);
+    position: relative;
+  }
+
+  & > * {
+    overflow: hidden;
+    position: absolute;
+    top: 0;
+    right: 0;
+    bottom: 0;
+    left: 0;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+  }
+
+  & > img,
+  & > video {
+    width: 100%;
+    height: 100%;
+    object-fit: cover;
+  }
+`
