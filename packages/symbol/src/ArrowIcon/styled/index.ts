89b1d1373de36aa22f5947d3250ee42c64e9fa27
Kanno Ami (tomixy) <tomix05may@gmail.com>
2022-08-24 19:59:09 +0900

    [ArrowIcon] 不要なimportを削除

--- packages/symbol/src/ArrowIcon/styled/index.ts
+++ packages/symbol/src/ArrowIcon/styled/index.ts
@@ -1,7 +1,6 @@
 import styled, { css } from 'styled-components'
 import { CharacterProps } from '../model/props'
 import { match } from 'ts-pattern'
-import { ColorPalette } from 'styled-utility-first'
 
 const rotate = (direction: CharacterProps['direction']) => {
   return match(direction)
