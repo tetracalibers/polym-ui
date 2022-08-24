644b8e0c1555f3e438f9d5c8c6f288ecdff93c04
Kanno Ami (tomixy) <tomix05may@gmail.com>
2022-08-24 18:23:50 +0900

    [HaveSidebar] スタイル定義

--- packages/layout/src/HaveSidebar/styled/index.ts
+++ packages/layout/src/HaveSidebar/styled/index.ts
@@ -2,6 +2,62 @@ import styled, { css } from 'styled-components'
 import { CharacterProps } from '../model/props'
 import { ElementType } from 'react'
 import { match } from 'ts-pattern'
-import { ColorPalette } from 'styled-utility-first'
+import { ColorPalette, Truthy } from 'styled-utility-first'
 
-export const StyledElement = styled.div``
+const sidebarStyle = (sideMaxWidth: CharacterProps['sideMaxWidth']) => {
+  return css`
+    /* サイドバーらしい幅 */
+    flex-basis: 20rem;
+    flex-grow: 1;
+    max-width: ${sideMaxWidth};
+  `
+}
+
+const mainStyle = (mainMinWidth: CharacterProps['mainMinWidth']) => {
+  return css`
+    flex-basis: 0;
+    flex-grow: 999;
+    min-width: ${mainMinWidth}%;
+  `
+}
+
+export const StyledElement = styled.div<CharacterProps>`
+  && {
+    display: flex;
+    flex-wrap: wrap;
+    gap: ${({ spaceBetween }) => spaceBetween};
+  }
+
+  ${({ noStretch }) => {
+    return Truthy(noStretch).when(css`
+      && {
+        align-items: flex-start;
+      }
+    `)
+  }}
+
+  ${({ sidebarChild, sideMaxWidth, mainMinWidth }) => {
+    return match(sidebarChild)
+      .with('first', () => {
+        return css`
+          & > :first-child {
+            ${sidebarStyle(sideMaxWidth)}
+          }
+          & > :last-child {
+            ${mainStyle(mainMinWidth)}
+          }
+        `
+      })
+      .with('last', () => {
+        return css`
+          & > :first-child {
+            ${mainStyle(mainMinWidth)}
+          }
+          & > :last-child {
+            ${sidebarStyle(sideMaxWidth)}
+          }
+        `
+      })
+      .exhaustive()
+  }}
+`
