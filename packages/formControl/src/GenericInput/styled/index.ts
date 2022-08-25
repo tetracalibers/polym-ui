f7f3a56f841513da50a58aaadf1125c70414efbe
Kanno Ami (tomixy) <tomix05may@gmail.com>
2022-08-25 14:22:46 +0900

    [formControl -> styled] &&による優先度調整

--- packages/formControl/src/GenericInput/styled/index.ts
+++ packages/formControl/src/GenericInput/styled/index.ts
@@ -1,22 +1,21 @@
-import styled, { css } from 'styled-components'
-import { CharacterProps } from '../model/props'
-import { ElementType } from 'react'
-import { match } from 'ts-pattern'
+import styled from 'styled-components'
 import { ColorPalette } from 'styled-utility-first'
 
 export const StyledInput = styled.input`
-  appearance: none;
-  width: 100%;
-  max-width: 300px;
-  padding: 11px 12px 12px 9px;
-  box-sizing: border-box;
-  outline: none;
-  height: 38px;
-  border-radius: 4px;
-  border: 1px solid ${ColorPalette.pastel.purple};
-  /* 16px以上を推奨 */
-  font-size: 16px;
-  line-height: 19px;
+  && {
+    appearance: none;
+    width: 100%;
+    max-width: 300px;
+    padding: 11px 12px 12px 9px;
+    box-sizing: border-box;
+    outline: none;
+    height: 38px;
+    border-radius: 4px;
+    border: 1px solid ${ColorPalette.pastel.purple};
+    /* 16px以上を推奨 */
+    font-size: 16px;
+    line-height: 19px;
+  }
 
   &::-webkit-outer-spin-button,
   &::-webkit-inner-spin-button {
--- packages/formControl/src/TextArea/styled/index.ts
+++ packages/formControl/src/TextArea/styled/index.ts
@@ -1,7 +1,5 @@
 import styled, { css } from 'styled-components'
 import { CharacterProps } from '../model/props'
-import { ElementType } from 'react'
-import { match } from 'ts-pattern'
 import { ColorPalette } from 'styled-utility-first'
 
 const border = (dangerOn: CharacterProps['hasError']) => {
@@ -15,18 +13,20 @@ const border = (dangerOn: CharacterProps['hasError']) => {
 }
 
 export const StyledElement = styled.textarea<CharacterProps>`
-  color: ${ColorPalette.grayScale.dark};
-  ${({ hasError }) => border(hasError)}
-  border-radius: 5px;
-  box-sizing: border-box;
-  outline: none;
-  width: 100%;
-  max-width: 300px;
-  font-size: 16px;
-  line-height: ${({ lineHeight }) => lineHeight}px;
-  padding: 9px 12px 10px 12px;
-  resize: none;
-  overflow: auto;
-  height: auto;
-  background-color: white;
+  && {
+    color: ${ColorPalette.grayScale.dark};
+    ${({ hasError }) => border(hasError)}
+    border-radius: 5px;
+    box-sizing: border-box;
+    outline: none;
+    width: 100%;
+    max-width: 300px;
+    font-size: 16px;
+    line-height: ${({ lineHeight }) => lineHeight}px;
+    padding: 9px 12px 10px 12px;
+    resize: none;
+    overflow: auto;
+    height: auto;
+    background-color: white;
+  }
 `
