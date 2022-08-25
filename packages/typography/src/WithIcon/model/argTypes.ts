d34c3768090fa1ba13c5902b8f479d26c2d19b9b
Kanno Ami (tomixy) <tomix05may@gmail.com>
2022-08-25 16:51:45 +0900

    [WithIcon] define props

--- packages/typography/src/WithIcon/model/argTypes.ts
+++ packages/typography/src/WithIcon/model/argTypes.ts
@@ -1,21 +1,57 @@
-import { defaultProps } from './props'
+import { defaultProps, iconChildOptions } from './props'
 import { CssStyle } from 'ts-typedef-helper'
 
 export const logicArgTypes = {
-  // recursive: {
-  //   control: {
-  //     type: 'boolean',
-  //   },
-  //   description: 'Whether to apply layout to nested elements',
-  //   table: {
-  //     type: {
-  //       summary: null,
-  //     },
-  //     category: 'style control',
-  //     defaultValue: {
-  //       summary: '',
-  //       details: null,
-  //     },
-  //   },
-  // },
+  iconChild: {
+    control: {
+      type: 'select',
+    },
+    options: iconChildOptions,
+    description:
+      'String indicating which of the two child elements is considered the icon',
+    table: {
+      type: {
+        summary: null,
+      },
+      category: 'character',
+      defaultValue: {
+        summary: defaultProps.iconChild,
+        details: null,
+      },
+    },
+  },
+  spaceV: {
+    control: {
+      type: 'number',
+    },
+    description:
+      'Numeric value representing the size of the gap between the icon and the text',
+    table: {
+      type: {
+        summary: null,
+      },
+      category: 'style control',
+      defaultValue: {
+        summary: defaultProps.spaceV,
+        details: null,
+      },
+    },
+  },
+  spaceU: {
+    control: {
+      type: 'select',
+    },
+    options: CssStyle.Unit.length,
+    description: 'Units of spaceV',
+    table: {
+      type: {
+        summary: null,
+      },
+      category: 'style control',
+      defaultValue: {
+        summary: defaultProps.spaceU,
+        details: null,
+      },
+    },
+  },
 }
--- packages/typography/src/WithIcon/model/props.ts
+++ packages/typography/src/WithIcon/model/props.ts
@@ -8,7 +8,14 @@ import { commonDefaultProps, CommonProps } from '../../common/props'
 import { styleDefaultProps, StyleProps } from '../css-props/props'
 import { CssStyle } from 'ts-typedef-helper'
 
-const conf = {}
+export const iconChildOptions = ['first', 'last'] as const
+export type IconChildOptions = typeof iconChildOptions[number]
+
+const conf = {
+  iconChild: NotRequired<IconChildOptions>('first'),
+  spaceV: NotRequired<number>(0.75),
+  spaceU: NotRequired<CssStyle.Unit.Length>('em'),
+}
 type Conf = typeof conf
 
 export type CharacterProps = getPropType<Conf> & CommonProps & StyleProps
