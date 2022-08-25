cb7348b2346a0f4d459ce4a21fd3207a9b27b694
Kanno Ami (tomixy) <tomix05may@gmail.com>
2022-08-25 19:13:53 +0900

    [layout] ビルドによるパッケージ更新

--- packages/layout/src/index.ts
+++ packages/layout/src/index.ts
@@ -3,6 +3,11 @@ import { HaveSidebar } from './HaveSidebar'
 import { HorizontalStack } from './HorizontalStack'
 import { VerticalStack } from './VerticalStack'
 import { HorizontalCenter } from './HorizontalCenter'
+import { SliderAlign } from './SliderAlign'
+import { Grid } from './Grid'
+import { OverlapLayer } from './OverlapLayer'
+import { AspectFrame } from './AspectFrame'
+import { DifferStack } from './DifferStack'
 
 export {
   VerticalCenter,
@@ -10,4 +15,9 @@ export {
   HorizontalStack,
   HorizontalCenter,
   VerticalStack,
+  SliderAlign,
+  Grid,
+  OverlapLayer,
+  AspectFrame,
+  DifferStack,
 }
