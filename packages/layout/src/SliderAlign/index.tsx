79f0f5dc3765c7f7979a0e1f1b34d5580c564ff9
Kanno Ami (tomixy) <tomix05may@gmail.com>
2022-08-25 11:59:02 +0900

    [SliderAlign] generate template

---
+++ packages/layout/src/SliderAlign/index.tsx
@@ -0,0 +1,31 @@
+import _ from 'lodash'
+import { ElementType, forwardRef, ReactElement } from 'react'
+import {
+  PolymorphicComponentPropWithRef,
+  PolymorphicRef,
+} from '../common/polymorphic/standard'
+import { CharacterProps, defaultProps } from './model/props'
+import { StyledElement } from './styled'
+
+export type SliderAlignProps<As extends ElementType> =
+  PolymorphicComponentPropWithRef<As, CharacterProps>
+
+export type SliderAlignComponent = <As extends ElementType>(
+  props: SliderAlignProps<As>
+) => ReactElement | null
+
+export const SliderAlign: SliderAlignComponent = forwardRef(
+  <As extends ElementType>(
+    { as, children, ..._props }: SliderAlignProps<As>,
+    ref?: PolymorphicRef<As>
+  ) => {
+    const props = _.mergeWith(_props, defaultProps, (input, defaul) =>
+      _.isUndefined(input) ? defaul : input
+    )
+    return (
+      <StyledElement {...props} ref={ref} as={as as unknown as undefined}>
+        {children}
+      </StyledElement>
+    )
+  }
+)
