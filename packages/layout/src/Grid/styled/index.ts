4b99697204e46b3cbfc086ccaf681538287c0a5a
Kanno Ami (tomixy) <tomix05may@gmail.com>
2022-08-25 11:44:35 +0900

    [Grid] スタイル定義

--- packages/layout/src/Grid/styled/index.ts
+++ packages/layout/src/Grid/styled/index.ts
@@ -1,7 +1,12 @@
-import styled, { css } from 'styled-components'
+import styled from 'styled-components'
 import { CharacterProps } from '../model/props'
-import { ElementType } from 'react'
-import { match } from 'ts-pattern'
-import { ColorPalette } from 'styled-utility-first'
 
-export const StyledElement = styled.div<CharacterProps>``
+export const StyledElement = styled.div<CharacterProps>`
+  --gap: ${({ spaceV, spaceU }) => `${spaceV}${spaceU}`};
+  --min: ${({ columnMinWidthV, columnMinWidthU }) =>
+    `${columnMinWidthV}${columnMinWidthU}`};
+
+  display: grid;
+  grid-gap: var(--gap);
+  grid-template-columns: repeat(auto-fit, minmax(min(var(--min), 100%), 1fr));
+`
