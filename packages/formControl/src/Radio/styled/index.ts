447f7da0f3483b5e034d559eb96b6f851f92e5b1
Kanno Ami (tomixy) <tomix05may@gmail.com>
2022-08-26 07:45:56 +0900

    [Radio] input要素を隠すCSSを記述

--- packages/formControl/src/Radio/styled/index.ts
+++ packages/formControl/src/Radio/styled/index.ts
@@ -5,6 +5,11 @@ import { ColorPalette, Truthy } from 'styled-utility-first'
 
 export const Label = styled.label``
 
-export const Input = styled.input<CharacterProps>``
+export const Input = styled.input<CharacterProps>`
+  /* 透明にして見えなくする */
+  opacity: 0;
+  /* 本来の配置から切り離す */
+  position: absolute;
+`
 
 export const Circle = styled.span``
