09f4f89c33c0448f080e297ba5999a5a239b2e2c
Kanno Ami (tomixy) <tomix05may@gmail.com>
2022-08-25 09:30:13 +0900

    [OverlapLayer -> styled] 未使用importを削除

--- packages/layout/src/OverlapLayer/styled/index.ts
+++ packages/layout/src/OverlapLayer/styled/index.ts
@@ -1,8 +1,6 @@
 import styled, { css } from 'styled-components'
 import { CharacterProps } from '../model/props'
-import { ElementType } from 'react'
-import { match } from 'ts-pattern'
-import { ColorPalette, Truthy } from 'styled-utility-first'
+import { Truthy } from 'styled-utility-first'
 
 export const StyledContainer = styled.div<CharacterProps>`
   position: relative;
