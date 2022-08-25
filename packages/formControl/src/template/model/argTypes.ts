7f85446b244cbb3cde35c853a3e7ecaad020832a
Kanno Ami (tomixy) <tomix05may@gmail.com>
2022-08-25 15:08:15 +0900

    [formControl] template更新

---
+++ packages/formControl/src/template/model/argTypes.ts
@@ -0,0 +1,21 @@
+import { defaultProps } from './props'
+import { CssStyle } from 'ts-typedef-helper'
+
+export const logicArgTypes = {
+  // recursive: {
+  //   control: {
+  //     type: 'boolean',
+  //   },
+  //   description: 'Whether to apply layout to nested elements',
+  //   table: {
+  //     type: {
+  //       summary: null,
+  //     },
+  //     category: 'style control',
+  //     defaultValue: {
+  //       summary: '',
+  //       details: null,
+  //     },
+  //   },
+  // },
+}
--- packages/formControl/src/template/storybook/componentName.stories.tsx
+++ packages/formControl/src/template/storybook/componentName.stories.tsx
@@ -3,6 +3,7 @@ import { WillFade } from '..'
 import { commmonArgTypes } from '../../common/argTypes'
 import { DarkTextBox } from '../../mock/TestBox'
 import { styleArgTypes } from '../css-props/argTypes'
+import { logicArgTypes } from '../model/argTypes'
 import { defaultProps } from '../model/props'
 import { DocsPage } from './docsPage'
 
@@ -12,12 +13,15 @@ export default {
   parameters: {
     docs: {
       page: () => <DocsPage />,
+      description: {
+        component: '',
+      },
     },
   },
   argTypes: {
     children: {
       control: {
-        type: 'text',
+        type: null,
       },
       description: 'Child elements of the element specified by as props',
       table: {
@@ -30,25 +34,7 @@ export default {
         required: true,
       },
     },
-    fadeFrom: {
-      control: {
-        type: null,
-      },
-      description: 'Which direction the elements appear from',
-      table: {
-        type: {
-          summary: null,
-        },
-        category: 'character',
-        defaultValue: {
-          summary: '', // TODO
-          details: null,
-        },
-      },
-      type: {
-        required: true,
-      },
-    },
+    ...logicArgTypes,
     ...commmonArgTypes,
     ...styleArgTypes,
   },
@@ -63,6 +49,5 @@ const Template: ComponentStory<typeof WillFade> = ({ children, ...args }) => (
 export const playground = Template.bind({})
 playground.args = {
   ...defaultProps,
-  children: 'Fade!!',
 }
 playground.argTypes = {}
