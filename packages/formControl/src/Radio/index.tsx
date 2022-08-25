436e85b836eed96435cdf497802a0045af04a825
Kanno Ami (tomixy) <tomix05may@gmail.com>
2022-08-26 07:40:43 +0900

    [Radio] asを廃止し、type属性をradioに固定

--- packages/formControl/src/Radio/index.tsx
+++ packages/formControl/src/Radio/index.tsx
@@ -1,31 +1,23 @@
 import _ from 'lodash'
-import { ElementType, forwardRef, ReactElement } from 'react'
-import {
-  PolymorphicComponentPropWithRef,
-  PolymorphicRef,
-} from '../common/polymorphic/standard'
+import { forwardRef, ReactElement } from 'react'
+import { InputComponentPropWithRef } from '../common/polymorphic/fixedAs'
+import { PolymorphicRef } from '../common/polymorphic/standard'
 import { CharacterProps, defaultProps } from './model/props'
-import { StyledElement } from './styled'
+import { StyledInput } from './styled'
 
-export type RadioProps<As extends ElementType> =
-  PolymorphicComponentPropWithRef<As, CharacterProps>
+export type RadioProps = Omit<InputComponentPropWithRef<CharacterProps>, 'type'>
 
-export type RadioComponent = <As extends ElementType>(
-  props: RadioProps<As>
-) => ReactElement | null
+export type RadioComponent = (props: RadioProps) => ReactElement | null
 
 export const Radio: RadioComponent = forwardRef(
-  <As extends ElementType>(
-    { as, children, ..._props }: RadioProps<As>,
-    ref?: PolymorphicRef<As>
-  ) => {
+  ({ children, ..._props }: RadioProps, ref?: PolymorphicRef<'input'>) => {
     const props = _.mergeWith(_props, defaultProps, (input, defaul) =>
       _.isUndefined(input) ? defaul : input
     )
     return (
-      <StyledElement {...props} ref={ref} as={as as unknown as undefined}>
+      <StyledInput {...props} ref={ref} type='radio'>
         {children}
-      </StyledElement>
+      </StyledInput>
     )
   }
 )
