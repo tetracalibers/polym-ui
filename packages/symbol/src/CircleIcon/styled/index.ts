059a1683d7cb2ef4e9222ca08fecf8116e546a38
Kanno Ami (tomixy) <tomix05may@gmail.com>
2022-08-26 08:18:06 +0900

    [CircleIcon] 仮スタイルで表示確認

--- packages/symbol/src/CircleIcon/styled/index.ts
+++ packages/symbol/src/CircleIcon/styled/index.ts
@@ -3,4 +3,13 @@ import { CharacterProps } from '../model/props'
 import { match } from 'ts-pattern'
 import { ColorPalette, Truthy } from 'styled-utility-first'
 
-export const StyledElement = styled.span<CharacterProps>``
+export const StyledElement = styled.span<CharacterProps>`
+  --width: 100px;
+  --background-color: ${ColorPalette.grayScale.dark};
+
+  display: inline-block;
+  width: var(--width);
+  height: var(--width);
+  border-radius: 50%;
+  background-color: var(--background-color);
+`
