b720579308a1e8775456d6f721b157c1ab83f10f
Kanno Ami (tomixy) <tomix05may@gmail.com>
2022-08-26 12:56:03 +0900

    [VisuallyHidden] スタイル定義

--- packages/a11y-helper/src/VisuallyHidden/styled/index.ts
+++ packages/a11y-helper/src/VisuallyHidden/styled/index.ts
@@ -1,6 +1,15 @@
-import styled, { css } from 'styled-components'
+import styled from 'styled-components'
 import { CharacterProps } from '../model/props'
-import { match } from 'ts-pattern'
-import { ColorPalette as $, Truthy } from 'styled-utility-first'
 
-export const StyledElement = styled.div<CharacterProps>``
+export const StyledElement = styled.div<CharacterProps>`
+  && {
+    border: 0;
+    clip: rect(0 0 0 0);
+    height: 1px;
+    margin: -1px;
+    overflow: hidden;
+    padding: 0;
+    position: absolute;
+    width: 1px;
+  }
+`
