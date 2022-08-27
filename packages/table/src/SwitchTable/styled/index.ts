d1f4fe880cbcd1166550133983aa8bb781eb95b6
Kanno Ami (tomixy) <tomix05may@gmail.com>
2022-08-28 00:45:20 +0900

    [SwitchTable] 横長画面でのレイアウトを実現

--- packages/table/src/SwitchTable/styled/index.ts
+++ packages/table/src/SwitchTable/styled/index.ts
@@ -4,4 +4,19 @@ import { match } from 'ts-pattern'
 import { ColorPalette as $, Truthy } from 'styled-utility-first'
 import { BaseTable } from '../../mock/BaseTable'
 
-export const Table = styled(BaseTable)``
+export const Table = styled(BaseTable)`
+  display: flex;
+
+  & tr {
+    display: block;
+    width: 20%;
+    box-sizing: border-box;
+  }
+
+  & th,
+  & td {
+    display: block;
+    width: 100%;
+    box-sizing: border-box;
+  }
+`
