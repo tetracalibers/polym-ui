8016fabe933357b5e79841115b0d6341a7b41d42
Kanno Ami (tomixy) <tomix05may@gmail.com>
2022-08-27 10:47:08 +0900

    [TextArea] resize処理をhookに分離

--- packages/formControl/src/TextArea/index.tsx
+++ packages/formControl/src/TextArea/index.tsx
@@ -1,13 +1,8 @@
 import _ from 'lodash'
-import {
-  ChangeEvent,
-  forwardRef,
-  ReactElement,
-  useCallback,
-  useState,
-} from 'react'
+import { forwardRef, ReactElement } from 'react'
 import { TextAreaComponentPropWithRef } from '../common/polymorphic/fixedAs'
 import { PolymorphicRef } from '../common/polymorphic/standard'
+import { useStretchableTextArea } from './hooks/useStretchableTextArea'
 import { CharacterProps, defaultProps } from './model/props'
 import { StyledTextarea } from './styled'
 
@@ -23,46 +18,10 @@ export const TextArea: TextAreaComponent = forwardRef(
     const props = _.mergeWith(_props, defaultProps, (input, defaul) =>
       _.isUndefined(input) ? defaul : input
     )
-    const {
-      onChange,
-      minRows,
-      maxRows,
-      lineHeight,
-      placeholder,
-      name,
-      id,
-      ...other
-    } = props
-
-    const [textareaRows, setTextareaRows] = useState(
-      Math.min(minRows!, maxRows!)
-    )
-
-    // 最大行数まで改行した時に高さが増える仕組み
-    // 入力が変わったときにe.target.scrollHeightから現在表示している行数を把握
-    // -> setTextareaで最大行数maxRowsを超えないように動的に変更
-    const changeHeight = useCallback(
-      (e: ChangeEvent<HTMLTextAreaElement>) => {
-        const previewsRows = e.target.rows
-        // 行数のリセット
-        e.target.rows = minRows!
-        // 現在の行数
-        const currentRows = Math.floor(e.target.scrollHeight / lineHeight!)
-        // 変化がなければ
-        if (currentRows === previewsRows) {
-          e.target.rows = currentRows
-        }
-        // 最大を超えたら
-        if (currentRows >= maxRows!) {
-          e.target.rows = maxRows!
-          e.target.scrollTop = e.target.scrollHeight
-        }
-        // 最大を超えないように行数をセット
-        setTextareaRows(Math.min(currentRows, maxRows!))
-        onChange && onChange(e)
-      },
-      [onChange, minRows, maxRows]
-    )
+    // prettier-ignore
+    const { onChange, minRows, maxRows, lineHeight, placeholder, name, id, ...other } = props
+    // prettier-ignore
+    const [textareaRows, changeHeight] = useStretchableTextArea(minRows!, maxRows!, lineHeight!, onChange)
 
     return (
       <>
