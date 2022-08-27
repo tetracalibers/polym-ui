c61d8883a57ffc6715fdbcfb8b0fdc1399ec489d
Kanno Ami (tomixy) <tomix05may@gmail.com>
2022-08-27 23:32:52 +0900

    [table] BaseとなるCSSを共有フォルダで分離

--- packages/table/src/ToListTable/styled/index.ts
+++ packages/table/src/ToListTable/styled/index.ts
@@ -2,28 +2,7 @@ import styled, { css } from 'styled-components'
 import { CharacterProps } from '../model/props'
 import { match } from 'ts-pattern'
 import { ColorPalette as $, Truthy } from 'styled-utility-first'
-
-export const BaseTable = styled.table<CharacterProps>`
-  & {
-    width: 100%;
-    border-collapse: collapse;
-  }
-
-  & th,
-  & td {
-    padding: 1em;
-    background-color: white;
-    color: ${$.grayScale.dark};
-    box-shadow: rgba(0, 0, 0, 0.1) 0px 0px 5px 0px,
-      rgba(0, 0, 0, 0.1) 0px 0px 1px 0px;
-  }
-
-  & th {
-    background-color: ${$.grayScale.dark};
-    color: ${$.grayScale.light};
-    padding-left: 1em;
-  }
-`
+import { BaseTable } from '../../mock/BaseTable'
 
 export const Table = styled(BaseTable)`
   tr {
