0c5df79743c58e564827261cb63a06a6eee5d4ab
Kanno Ami (tomixy) <tomix05may@gmail.com>
2022-08-28 13:18:39 +0900

    [Tab] アクセシビリティを考慮してマークアップ改良

--- packages/hide-and-seek/src/Tab/index.tsx
+++ packages/hide-and-seek/src/Tab/index.tsx
@@ -27,24 +27,71 @@ export const Tab: TabComponent = forwardRef(
     )
     return (
       <Wrapper {...props} ref={ref} as={as as unknown as undefined}>
-        <ul>
-          <li>
-            <a>Tab 1</a>
+        <ul role='tablist'>
+          <li role='presentation'>
+            <a
+              href='#panel01'
+              role='tab'
+              aria-controls='panel01'
+              aria-selected='true'
+              aria-expanded='true'
+              id='tab01'
+            >
+              Tab 1
+            </a>
           </li>
-          <li>
-            <a>Tab 2</a>
+          <li role='presentation'>
+            <a
+              href='#panel02'
+              role='tab'
+              aria-controls='panel02'
+              aria-selected='false'
+              aria-expanded='false'
+              tabIndex={-1}
+              id='tab02'
+            >
+              Tab 2
+            </a>
           </li>
-          <li>
-            <a>Tab 3</a>
+          <li role='presentation'>
+            <a
+              href='#panel03'
+              role='tab'
+              aria-controls='panel03'
+              aria-selected='false'
+              aria-expanded='false'
+              tabIndex={-1}
+              id='tab03'
+            >
+              Tab 3
+            </a>
           </li>
         </ul>
-        <div>
+        <div
+          id='panel01'
+          aria-labelledby='tab01'
+          role='tabpanel'
+          aria-hidden='false'
+        >
+          <h2 tabIndex={0}>Panel 1</h2>
           <DummyText500W />
         </div>
-        <div>
+        <div
+          id='panel02'
+          aria-labelledby='tab02'
+          role='tabpanel'
+          aria-hidden='true'
+        >
+          <h2 tabIndex={0}>Panel 2</h2>
           <DummyText500W />
         </div>
-        <div>
+        <div
+          id='panel03'
+          aria-labelledby='tab03'
+          role='tabpanel'
+          aria-hidden='true'
+        >
+          <h2 tabIndex={0}>Panel 3</h2>
           <DummyText500W />
         </div>
       </Wrapper>
