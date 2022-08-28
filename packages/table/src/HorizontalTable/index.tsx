3747f544ffa79ef51cc073677cfda48f052636a4
Kanno Ami (tomixy) <tomix05may@gmail.com>
2022-08-28 17:14:04 +0900

    [HorizontalTable] Tableコンポーネントを用いて書き換え

--- packages/table/src/HorizontalTable/index.tsx
+++ packages/table/src/HorizontalTable/index.tsx
@@ -1,55 +1,35 @@
 import _ from 'lodash'
-import { forwardRef, ReactElement } from 'react'
-import { TableComponentPropWithRef } from '../common/polymorphic/fixedAs'
-import { PolymorphicRef } from '../common/polymorphic/standard'
+import { ReactElement, ReactNode } from 'react'
+import { TableComponentProp } from '../common/polymorphic/fixedAs'
 import { CharacterProps, defaultProps } from './model/props'
-import { Table, Wrapper } from './styled'
-import { VerticalStack } from '@polym-ui/layout'
+import { LayoutTable, Wrapper } from './styled'
+import { Table } from '../core/Table'
+import { generateTableData } from '../mock/data'
 
-export type HorizontalTableProps = TableComponentPropWithRef<CharacterProps>
+export type HorizontalTableProps = TableComponentProp<CharacterProps>
 
 export type HorizontalTableComponent = (
   props: HorizontalTableProps
-) => ReactElement | null
+) => ReactElement
 
-export const HorizontalTable: HorizontalTableComponent = forwardRef(
-  (
-    { children, ..._props }: HorizontalTableProps,
-    ref?: PolymorphicRef<'table'>
-  ) => {
-    const props = _.mergeWith(_props, defaultProps, (input, defaul) =>
-      _.isUndefined(input) ? defaul : input
-    )
-    return (
-      <Wrapper>
-        <Table {...props} ref={ref}>
-          <tr>
-            <th>Heading Cell 1</th>
-            <td>Data Cell 1</td>
-            <td>Data Cell 1</td>
-          </tr>
-          <tr>
-            <th>Heading Cell 2</th>
-            <td>Data Cell 2</td>
-            <td>Data Cell 2</td>
-          </tr>
-          <tr>
-            <th>Heading Cell 3</th>
-            <td>Data Cell 3</td>
-            <td>Data Cell 3</td>
-          </tr>
-          <tr>
-            <th>Heading Cell 4</th>
-            <td>Data Cell 4</td>
-            <td>Data Cell 4</td>
-          </tr>
-          <tr>
-            <th>Heading Cell 5</th>
-            <td>Data Cell 5</td>
-            <td>Data Cell 5</td>
-          </tr>
-        </Table>
-      </Wrapper>
-    )
-  }
-)
+const testData = generateTableData()
+
+export const HorizontalTable: HorizontalTableComponent = ({
+  children,
+  ..._props
+}: HorizontalTableProps) => {
+  const props = _.mergeWith(_props, defaultProps, (input, defaul) =>
+    _.isUndefined(input) ? defaul : input
+  )
+  return (
+    <Wrapper>
+      <Table
+        {...props}
+        data={testData}
+        format={{
+          table: (children: ReactNode) => <LayoutTable>{children}</LayoutTable>,
+        }}
+      />
+    </Wrapper>
+  )
+}
--- packages/table/src/HorizontalTable/styled/index.ts
+++ packages/table/src/HorizontalTable/styled/index.ts
@@ -1,7 +1,4 @@
-import styled, { css } from 'styled-components'
-import { CharacterProps } from '../model/props'
-import { match } from 'ts-pattern'
-import { ColorPalette as $, Truthy } from 'styled-utility-first'
+import styled from 'styled-components'
 import { BaseTable } from '../../mock/BaseTable'
 
 export const Wrapper = styled.div`
@@ -9,9 +6,9 @@ export const Wrapper = styled.div`
   overflow-x: auto;
 `
 
-export const Table = styled(BaseTable)`
-  & th,
-  & td {
+export const LayoutTable = styled(BaseTable)`
+  & tbody th,
+  & tbody td {
     white-space: nowrap;
   }
 `
