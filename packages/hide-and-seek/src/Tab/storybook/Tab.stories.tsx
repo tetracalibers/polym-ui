00749d146d8fb602f8c602a0ff946fcd2b434081
Kanno Ami (tomixy) <tomix05may@gmail.com>
2022-08-28 18:46:17 +0900

    [Tab] storyを仮定義

--- packages/hide-and-seek/src/Tab/storybook/Tab.stories.tsx
+++ packages/hide-and-seek/src/Tab/storybook/Tab.stories.tsx
@@ -1,15 +1,24 @@
 import { ComponentStory } from '@storybook/react'
-import { Tab } from '..'
 import { commmonArgTypes } from '../../common/argTypes'
 import { DarkTextBox } from '../../mock/TestBox'
 import { styleArgTypes } from '../css-props/argTypes'
 import { logicArgTypes } from '../model/argTypes'
 import { defaultProps } from '../model/props'
 import { DocsPage } from './docsPage'
+import { nanoid } from 'nanoid'
+
+import { TabGroup } from '../core/TabGroup'
+import { DummyText50W } from '../../mock/DummyText'
+const { Tab, TabList, Panel } = TabGroup
 
 export default {
   title: 'hide-and-seek/Tab',
-  component: Tab,
+  component: TabGroup,
+  subcomponent: {
+    Tab,
+    TabList,
+    Panel,
+  },
   parameters: {
     docs: {
       page: () => <DocsPage />,
@@ -40,10 +49,28 @@ export default {
   },
 }
 
-const Template: ComponentStory<typeof Tab> = ({ children, ...args }) => (
-  <Tab {...args} as={DarkTextBox}>
-    {children}
-  </Tab>
+const seed = [...new Array(3)].map(() => nanoid())
+const generateTabId = (panelId: string) => `tab-for-${panelId}`
+
+const Template: ComponentStory<typeof TabGroup> = () => (
+  <TabGroup>
+    <TabList>
+      {seed.map((id, idx) => {
+        const thisId = generateTabId(id)
+        return (
+          <Tab id={thisId} key={thisId} panelId={id}>
+            Tab {idx + 1}
+          </Tab>
+        )
+      })}
+    </TabList>
+    {seed.map((id, idx) => (
+      <Panel key={id} tabId={generateTabId(id)} id={id}>
+        <h2>Panel {idx + 1}</h2>
+        <DummyText50W />
+      </Panel>
+    ))}
+  </TabGroup>
 )
 
 export const playground = Template.bind({})
