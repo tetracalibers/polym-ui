4726f161e56305967ba7c7dfa237cc1adcb8c509
Kanno Ami (tomixy) <tomix05may@gmail.com>
2022-08-28 11:00:24 +0900

    [Tab] generate template

---
+++ packages/hide-and-seek/src/Tab/index.tsx
@@ -0,0 +1,33 @@
+import _ from 'lodash'
+import { ElementType, forwardRef, ReactElement } from 'react'
+import {
+  PolymorphicComponentPropWithRef,
+  PolymorphicRef,
+} from '../common/polymorphic/standard'
+import { CharacterProps, defaultProps } from './model/props'
+import { StyledElement } from './styled'
+
+export type TabProps<As extends ElementType> = PolymorphicComponentPropWithRef<
+  As,
+  CharacterProps
+>
+
+export type TabComponent = <As extends ElementType>(
+  props: TabProps<As>
+) => ReactElement | null
+
+export const Tab: TabComponent = forwardRef(
+  <As extends ElementType>(
+    { as, children, ..._props }: TabProps<As>,
+    ref?: PolymorphicRef<As>
+  ) => {
+    const props = _.mergeWith(_props, defaultProps, (input, defaul) =>
+      _.isUndefined(input) ? defaul : input
+    )
+    return (
+      <StyledElement {...props} ref={ref} as={as as unknown as undefined}>
+        {children}
+      </StyledElement>
+    )
+  }
+)
---
+++ packages/hide-and-seek/src/Tab/storybook/Tab.stories.tsx
@@ -0,0 +1,53 @@
+import { ComponentStory } from '@storybook/react'
+import { Tab } from '..'
+import { commmonArgTypes } from '../../common/argTypes'
+import { DarkTextBox } from '../../mock/TestBox'
+import { styleArgTypes } from '../css-props/argTypes'
+import { logicArgTypes } from '../model/argTypes'
+import { defaultProps } from '../model/props'
+import { DocsPage } from './docsPage'
+
+export default {
+  title: 'hide-and-seek/Tab',
+  component: Tab,
+  parameters: {
+    docs: {
+      page: () => <DocsPage />,
+      description: {
+        component: '',
+      },
+    },
+  },
+  argTypes: {
+    children: {
+      control: {
+        type: null,
+      },
+      description: 'Child elements of the element specified by as props',
+      table: {
+        type: {
+          summary: null,
+        },
+        category: 'character',
+      },
+      type: {
+        required: true,
+      },
+    },
+    ...logicArgTypes,
+    ...commmonArgTypes,
+    ...styleArgTypes,
+  },
+}
+
+const Template: ComponentStory<typeof Tab> = ({ children, ...args }) => (
+  <Tab {...args} as={DarkTextBox}>
+    {children}
+  </Tab>
+)
+
+export const playground = Template.bind({})
+playground.args = {
+  ...defaultProps,
+}
+playground.argTypes = {}
