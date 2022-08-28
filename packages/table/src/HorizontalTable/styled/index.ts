104030194865e837d63790256328146371c20ea4
Kanno Ami (tomixy) <tomix05may@gmail.com>
2022-08-28 09:17:34 +0900

    [HorizontalTable] レスポンシブなレイアウト実現

--- packages/table/src/HorizontalTable/styled/index.ts
+++ packages/table/src/HorizontalTable/styled/index.ts
@@ -10,20 +10,8 @@ export const Wrapper = styled.div`
 `
 
 export const Table = styled(BaseTable)`
-  display: flex;
-  width: 100%;
-
-  & tr {
-    display: block;
-    box-sizing: border-box;
-    flex: 1;
-  }
-
   & th,
   & td {
-    display: block;
-    width: 100%;
-    box-sizing: border-box;
     white-space: nowrap;
   }
 `
