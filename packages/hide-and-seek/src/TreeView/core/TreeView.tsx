6d2f68ca8b131dcd9767752b9c9abcab6350c618
Kanno Ami (tomixy) <tomix05may@gmail.com>
2022-08-29 12:10:52 +0900

    [TreeView] generate template

---
+++ packages/hide-and-seek/src/TreeView/core/TreeView.tsx
@@ -0,0 +1,148 @@
+import _ from 'lodash'
+import {
+  Children,
+  ComponentPropsWithoutRef,
+  createContext,
+  Dispatch,
+  ReactNode,
+  SetStateAction,
+  useContext,
+  useEffect,
+  useState,
+} from 'react'
+import { useNanoId } from '../../hooks/useNanoId'
+import { useShareState } from '../../hooks/useShareState'
+import { DetailWrapper, SummaryButton } from '../styled'
+
+/* -------------------------------------------- */
+
+type AccordionProps = {
+  children: ReactNode
+  openOneOnly?: boolean
+}
+
+type AccordionState = {
+  openOneOnly: boolean
+  activePanelId: string[]
+  updateActivePanelId: Dispatch<SetStateAction<string[]>>
+}
+
+const AccordionContext = createContext({} as AccordionState)
+
+export const Accordion = ({
+  children,
+  openOneOnly = false,
+}: AccordionProps) => {
+  const [activePanelId, updateActivePanelId] = useState<string[]>([])
+
+  const state = useShareState(
+    { openOneOnly, activePanelId, updateActivePanelId },
+    [openOneOnly, activePanelId]
+  )
+
+  return (
+    <AccordionContext.Provider value={state}>
+      {children}
+    </AccordionContext.Provider>
+  )
+}
+
+/* -------------------------------------------- */
+
+type PanelInnerProps = {
+  children: ReactNode
+  detailId: string
+  summaryId: string
+  isOpen: boolean
+}
+
+const Summary = ({
+  children,
+  summaryId,
+  detailId,
+  isOpen,
+  ...attrs
+}: PanelInnerProps & ComponentPropsWithoutRef<'button'>) => {
+  return (
+    <SummaryButton
+      type='button'
+      aria-expanded={isOpen}
+      aria-controls={detailId}
+      id={summaryId}
+      {...attrs}
+    >
+      {children}
+    </SummaryButton>
+  )
+}
+
+/* -------------------------------------------- */
+
+const Detail = ({ children, detailId, summaryId, isOpen }: PanelInnerProps) => {
+  return (
+    <DetailWrapper
+      id={detailId}
+      role='region'
+      aria-labelledby={summaryId}
+      aria-hidden={!isOpen}
+      tabIndex={0}
+    >
+      {children}
+    </DetailWrapper>
+  )
+}
+
+/* -------------------------------------------- */
+
+type PanelProps = {
+  children: [ReactNode, ReactNode]
+}
+
+const Panel = ({ children }: PanelProps) => {
+  const { openOneOnly, updateActivePanelId, activePanelId } =
+    useContext(AccordionContext)
+
+  const detailId = useNanoId()
+  const summaryId = 'for-' + detailId
+
+  const [summary, detail] = Children.toArray(children)
+
+  const [isOpen, setOpenStatus] = useState(false)
+
+  useEffect(() => {
+    setOpenStatus(activePanelId.includes(detailId))
+  }, [activePanelId])
+
+  const open = () => {
+    if (openOneOnly) {
+      updateActivePanelId([detailId])
+    } else {
+      updateActivePanelId([...activePanelId, detailId])
+    }
+  }
+
+  const close = () => {
+    updateActivePanelId(_.without(activePanelId, detailId))
+  }
+
+  const toggleOpen = () => (isOpen ? close() : open())
+
+  const injectProps = {
+    detailId,
+    summaryId,
+    isOpen,
+  }
+
+  return (
+    <>
+      <Summary {...injectProps} onClick={toggleOpen}>
+        {summary}
+      </Summary>
+      <Detail {...injectProps}>{detail}</Detail>
+    </>
+  )
+}
+
+/* -------------------------------------------- */
+
+Accordion.Panel = Panel
---
+++ packages/hide-and-seek/src/TreeView/styled/index.ts
@@ -0,0 +1,19 @@
+import styled, { css } from 'styled-components'
+import { CharacterProps } from '../model/props'
+import { match } from 'ts-pattern'
+import { ColorPalette as $, ResetCss, Truthy } from 'styled-utility-first'
+
+export const DetailWrapper = styled.div<CharacterProps>`
+  &[aria-hidden='true'] {
+    display: none;
+  }
+`
+
+export const SummaryButton = styled.button`
+  ${ResetCss.button}
+  display: block;
+
+  &:focus {
+    outline: 2px solid ${$.pastel.water};
+  }
+`
