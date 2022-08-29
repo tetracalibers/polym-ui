b21c38724cc0b84e95a60c8a7f98f64bc67f29d3
Kanno Ami (tomixy) <tomix05may@gmail.com>
2022-08-30 08:56:54 +0900

    [Media] generate template

---
+++ packages/layout/src/Media/storybook/Media.stories.tsx
@@ -0,0 +1,53 @@
+import { ComponentStory } from '@storybook/react'
+import { Media } from '..'
+import { commmonArgTypes } from '../../common/argTypes'
+import { DarkTextBox } from '../../mock/TestBox'
+import { styleArgTypes } from '../css-props/argTypes'
+import { logicArgTypes } from '../model/argTypes'
+import { defaultProps } from '../model/props'
+import { DocsPage } from './docsPage'
+
+export default {
+  title: 'layout pettern/Media',
+  component: Media,
+  parameters: {
+    docs: {
+      page: () => <DocsPage />,
+      description: {
+        component: '',
+      },
+    },
+  },
+  argTypes: {
+    children: {
+      control: {
+        type: null,
+      },
+      description: 'Child elements of the element specified by as props',
+      table: {
+        type: {
+          summary: null,
+        },
+        category: 'character',
+      },
+      type: {
+        required: true,
+      },
+    },
+    ...logicArgTypes,
+    ...commmonArgTypes,
+    ...styleArgTypes,
+  },
+}
+
+const Template: ComponentStory<typeof Media> = ({ children, ...args }) => (
+  <Media {...args} as={DarkTextBox}>
+    {children}
+  </Media>
+)
+
+export const playground = Template.bind({})
+playground.args = {
+  ...defaultProps,
+}
+playground.argTypes = {}
