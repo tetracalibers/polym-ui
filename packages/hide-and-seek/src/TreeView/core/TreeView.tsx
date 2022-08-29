d1f23f8b2dbcfb8b6d56ac48e43574bf89eb8f4a
Kanno Ami (tomixy) <tomix05may@gmail.com>
2022-08-29 14:19:20 +0900

    [TreeView] Contextを定義し、format propsが表示に反映されるように

--- packages/hide-and-seek/src/TreeView/core/TreeView.tsx
+++ packages/hide-and-seek/src/TreeView/core/TreeView.tsx
@@ -1,9 +1,12 @@
 import {
   Children,
   ComponentPropsWithoutRef,
+  createContext,
   ReactElement,
   ReactNode,
+  useContext,
 } from 'react'
+import { useShareState } from '../../hooks/useShareState'
 
 /* -------------------------------------------- */
 
@@ -19,16 +22,27 @@ type TreeProps = {
   }
 }
 
-export const Tree = ({ children, label }: TreeProps) => {
+type ShareState = {
+  format: TreeProps['format']
+}
+
+const TreeContext = createContext<ShareState>({} as ShareState)
+
+export const Tree = ({ children, label, format }: TreeProps) => {
   const [root, child] = Children.toArray(children)
 
+  const shareState = useShareState({ format }, [])
+
+  const topRootFormatter = format?.topRoot
+  const topRoot = topRootFormatter ? topRootFormatter(root) : root
+
   return (
-    <>
-      <div id={label}>{root}</div>
+    <TreeContext.Provider value={shareState}>
+      <div id={label}>{topRoot}</div>
       <ul role='tree' aria-labelledby={label}>
         {child}
       </ul>
-    </>
+    </TreeContext.Provider>
   )
 }
 
@@ -41,9 +55,13 @@ type SubTreeProps = {
 const SubTree = ({ children }: SubTreeProps) => {
   const [root, child] = Children.toArray(children)
 
+  const { format } = useContext(TreeContext)
+  const subRootFormatter = format?.subRoot
+  const subRoot = subRootFormatter ? subRootFormatter(root) : root
+
   return (
     <li role='treeitem' aria-expanded={false} aria-selected={false}>
-      <div>{root}</div>
+      <div>{subRoot}</div>
       <ul role='group'>{child}</ul>
     </li>
   )
@@ -55,9 +73,13 @@ type LeafProps = {
 } & ComponentPropsWithoutRef<'li'>
 
 const Leaf = ({ children, ...attrs }: LeafProps) => {
+  const { format } = useContext(TreeContext)
+  const leafFormatter = format?.leaf
+  const leaf = leafFormatter ? leafFormatter(children) : children
+
   return (
     <li role='treeitem' {...attrs}>
-      {children}
+      {leaf}
     </li>
   )
 }
