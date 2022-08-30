871822c8126caeb2172e5e459429607b85f83fd6
Kanno Ami (tomixy) <tomix05may@gmail.com>
2022-08-30 15:01:11 +0900

    [ScaleImage] hover時に画像が拡大するeffectを実装

--- packages/image/src/components/effect/ScaleImage/styled/index.ts
+++ packages/image/src/components/effect/ScaleImage/styled/index.ts
@@ -3,4 +3,25 @@ import { CharacterProps } from '../model/props'
 import { match } from 'ts-pattern'
 import { ColorPalette as $, Truthy } from 'styled-utility-first'
 
-export const Mask = styled.span<CharacterProps>``
+export const Mask = styled.span<CharacterProps>`
+  --width: ${({ width }) => width};
+  --height: ${({ height }) => height};
+
+  /* はみ出す画像を隠す */
+  && {
+    display: block;
+    line-height: 0;
+    overflow: hidden;
+    width: var(--width);
+    height: var(--height);
+  }
+
+  && img {
+    transform: scale(1);
+    transition: 0.3s ease-in-out;
+  }
+
+  && img:hover {
+    transform: scale(1.2);
+  }
+`
