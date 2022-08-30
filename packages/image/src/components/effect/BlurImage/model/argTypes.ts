27299ecf72a21b4933c84b2006e7cf6a52254cb6
Kanno Ami (tomixy) <tomix05may@gmail.com>
2022-08-30 16:12:16 +0900

    [BlurImage] 不要なprops定義を削除

--- packages/image/src/components/effect/BlurImage/model/argTypes.ts
+++ packages/image/src/components/effect/BlurImage/model/argTypes.ts
@@ -1,94 +1,23 @@
-import { defaultProps, modeOptions, scaleFactorRange } from './props'
+import { defaultProps } from './props'
 import { CssStyle } from 'ts-typedef-helper'
 
 export const logicArgTypes = {
-  zoom: {
-    control: {
-      type: 'radio',
-    },
-    options: modeOptions,
-    description: '"in" means shrinking, "out" means expanding.',
-    table: {
-      type: {
-        summary: null,
-      },
-      category: 'effect',
-      subcategory: 'scale',
-      defaultValue: {
-        summary: defaultProps.zoom,
-        details: null,
-      },
-    },
-  },
-  withRotate: {
-    control: {
-      type: 'boolean',
-    },
-    description: 'Whether to rotate at the same time as scaling',
-    table: {
-      type: {
-        summary: null,
-      },
-      category: 'effect',
-      subcategory: 'rotate',
-      defaultValue: {
-        summary: defaultProps.withRotate,
-        details: null,
-      },
-    },
-  },
-  clockwise: {
-    control: {
-      type: 'boolean',
-    },
-    description: 'Clockwise if true, counterclockwise if false',
-    table: {
-      type: {
-        summary: null,
-      },
-      category: 'effect',
-      subcategory: 'rotate',
-      defaultValue: {
-        summary: defaultProps.clockwise,
-        details: null,
-      },
-    },
-  },
-  angle: {
-    control: {
-      type: 'number',
-    },
-    description:
-      'Angle of rotation. (Absolute values are used. Negative values do not cause counterclockwise rotation unless the "clockwise" props are set to false.)',
-    table: {
-      type: {
-        summary: null,
-      },
-      category: 'effect',
-      subcategory: 'rotate',
-      defaultValue: {
-        summary: defaultProps.angle,
-        details: null,
-      },
-    },
-  },
-  scaleFactor: {
-    control: {
-      type: 'select',
-    },
-    options: scaleFactorRange,
-    description:
-      'A value that controls the scaling factor of the element.\nIf zoom="in", the value is as is; if zoom="out", the value is the actual magnification factor plus 1.',
-    table: {
-      type: {
-        summary: null,
-      },
-      category: 'effect',
-      subcategory: 'scale',
-      defaultValue: {
-        summary: defaultProps.scaleFactor,
-        details: null,
-      },
-    },
-  },
+  // zoom: {
+  //   control: {
+  //     type: 'radio',
+  //   },
+  //   options: modeOptions,
+  //   description: '"in" means shrinking, "out" means expanding.',
+  //   table: {
+  //     type: {
+  //       summary: null,
+  //     },
+  //     category: 'effect',
+  //     subcategory: 'scale',
+  //     defaultValue: {
+  //       summary: defaultProps.zoom,
+  //       details: null,
+  //     },
+  //   },
+  // },
 }
