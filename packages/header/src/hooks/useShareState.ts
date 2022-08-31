27565808d048e14ef0920dcf15160e5ba47673dc
Kanno Ami (tomixy) <tomix05may@gmail.com>
2022-08-31 19:52:09 +0900

    [ui] headerパッケージを移行

--- packages/header/src/hooks/useShareState.ts
+++
@@ -1,6 +0,0 @@
-import { useMemo } from 'react'
-
-export const useShareState = <T>(publicState: T, dependentArray: unknown[]) => {
-  const shareState = useMemo<T>(() => publicState, dependentArray)
-  return shareState
-}
