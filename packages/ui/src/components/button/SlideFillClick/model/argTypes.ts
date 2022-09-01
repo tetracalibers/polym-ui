27848b15d0e05b56f565e609c50a80d5ce89a36c
Kanno Ami (tomixy) <tomix05may@gmail.com>
2022-09-01 15:28:38 +0900

    [SlideFillClick] slide propsのドキュメント作成

--- packages/ui/src/components/button/SlideFillClick/model/argTypes.ts
+++ packages/ui/src/components/button/SlideFillClick/model/argTypes.ts
@@ -1,4 +1,22 @@
-import { defaultProps } from './props'
+import { defaultProps, slideOptions } from './props'
 import { CssStyle } from 'ts-typedef-helper'
 
-export const thisArgTypes = {}
+export const thisArgTypes = {
+  slide: {
+    control: {
+      type: 'radio',
+    },
+    options: slideOptions,
+    description: 'Type of animation in which the background slides out',
+    table: {
+      category: 'effect',
+      subcategory: 'animation',
+      type: {
+        summary: null,
+      },
+      defaultValue: {
+        summary: defaultProps.slide,
+      },
+    },
+  },
+}
