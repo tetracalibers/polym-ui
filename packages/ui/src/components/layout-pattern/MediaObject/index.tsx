796960fb3b6dd2c8ade7841139bb528d99691a1f
Kanno Ami (tomixy) <tomix05may@gmail.com>
2022-09-02 18:20:48 +0900

    [MediaObject] 大まかなコンポーネント実装

--- packages/ui/src/components/layout-pattern/MediaObject/index.tsx
+++ packages/ui/src/components/layout-pattern/MediaObject/index.tsx
@@ -1,67 +1,16 @@
-/* eslint-disable no-undef */
-import _ from 'lodash'
-import {
-  ComponentProps,
-  ForwardedRef,
-  forwardRef,
-  ReactElement,
-  ReactNode,
-} from 'react'
-import { CharacterProps, defaultProps } from './model/props'
+import { ReactElement } from 'react'
+import { WithSidebar } from '../../layout-algorithm/WithSidebar'
 
-const allowTag = [
-  'address',
-  'section',
-  'article',
-  'div',
-  'dd',
-  'li',
-  'p',
-  'a',
-] as const
-type AllowTag = typeof allowTag[number]
-
-export type MediaObjectProps<As extends AllowTag> = {
-  ref?: ForwardedRef<HTMLElementTagNameMap[As]>
-  as?: As
-  children: ReactNode
-  media: ReactElement<any, 'img'>
-} & CharacterProps &
-  ComponentProps<As>
-
-export type MediaObjectComponent = <As extends AllowTag>(
-  props: MediaObjectProps<As>
-) => ReactElement | null
-
-export const MediaObjectInner = <As extends AllowTag>({
-  as,
-  children,
-  ref,
-  media,
-  ..._props
-}: MediaObjectProps<As>) => {
-  const props = _.mergeWith(_props, defaultProps, (input, defaul) =>
-    _.isUndefined(input) ? defaul : input
-  )
-
-  if (as === undefined) {
-    return (
-      <div
-        {...(props as Omit<MediaObjectProps<'div'>, 'as' | 'ref' | 'children'>)}
-        ref={ref as ForwardedRef<HTMLDivElement>}
-      >
-        {children}
-      </div>
-    )
-  }
-
-  const Tag = as as string
-  return (
-    <Tag {...props} ref={ref}>
-      {media}
-      {children}
-    </Tag>
-  )
+export type MediaObjectProps = {
+  children: ReactElement
+  media: ReactElement
 }
 
-export const MediaObject: MediaObjectComponent = forwardRef(MediaObjectInner)
+export const MediaObject = ({ children, media }: MediaObjectProps) => {
+  return (
+    <WithSidebar>
+      {media}
+      {children}
+    </WithSidebar>
+  )
+}
