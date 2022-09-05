5bed329675a4ee53d348f0ea58db4843132d211d
Kanno Ami (tomixy) <tomix05may@gmail.com>
2022-09-05 09:30:45 +0900

    [LinkList.Underline] hoverEffect="growFromCircle"の場合のeffect実装

--- packages/ui/src/components/LinkList/styled/underline.ts
+++ packages/ui/src/components/LinkList/styled/underline.ts
@@ -1,19 +1,65 @@
-import { LinkList, UnderLineLinkListProps } from '../index'
-import styled, { css } from 'styled-components'
+import { UnderLineLinkListProps } from '../index'
+import { css } from 'styled-components'
 import { ColorPalette as $ } from 'styled-utility-first'
 import { match } from 'ts-pattern'
-import { baseStyle, CoreUl } from './core'
 
 const setEffect = (type: UnderLineLinkListProps['hoverEffect']) => {
   return match(type)
     .with('growFromCenter', () => {
       return css`
-        transform-origin: center top;
+        & > li a::after {
+          transform-origin: center top;
+          transform: scale(0, 1); /*X方向0、Y方向1*/
+          bottom: 0;
+          left: 10%;
+          /*線の形状*/
+          width: 80%;
+          height: 2px;
+        }
+
+        & > li[data-active='true'] a::after,
+        & > li a:hover::after {
+          transform: scale(1, 1); /*X方向にスケール拡大*/
+        }
       `
     })
     .with('growFromLeft', () => {
       return css`
-        transform-origin: left top;
+        & > li a::after {
+          transform-origin: left top;
+          transform: scale(0, 1); /*X方向0、Y方向1*/
+          bottom: 0;
+          left: 10%;
+          /*線の形状*/
+          width: 80%;
+          height: 2px;
+        }
+
+        & > li[data-active='true'] a::after,
+        & > li a:hover::after {
+          transform: scale(1, 1); /*X方向にスケール拡大*/
+        }
+      `
+    })
+    .with('growFromCircle', () => {
+      return css`
+        & > li a::after {
+          transform-origin: center bottom; /*中央下部基点*/
+          bottom: 0;
+          left: 0;
+          /*線になる丸の形状*/
+          width: 100%;
+          height: 5px;
+          border-radius: 50%;
+          transform: scale(0.04, 1); /*X方向0.04、Y方向1*/
+        }
+
+        & > li[data-active='true'] a::after,
+        & > li a:hover::after {
+          height: 2px; /*縦幅を変化*/
+          border-radius: 0; /*丸みをなくす*/
+          transform: scale(0.8, 1); /*X方向0.8、Y方向1にスケール拡大*/
+        }
       `
     })
     .otherwise(() => '')
@@ -37,13 +83,6 @@ export const injectUnderlineStyle = css`
     background-color: var(--bg-color);
     /*アニメーションの指定*/
     transition: all var(--duration);
-    transform: scale(0, 1); /*X方向0、Y方向1*/
-    bottom: 0;
-    left: 10%;
-    /*線の形状*/
-    width: 80%;
-    height: 2px;
-    ${({ theme }) => setEffect(theme.hoverEffect)}
   }
 
   & > li[data-active='true'] a,
@@ -51,8 +90,5 @@ export const injectUnderlineStyle = css`
     color: var(--color);
   }
 
-  & > li[data-active='true'] a::after,
-  & > li a:hover::after {
-    transform: scale(1, 1); /*X方向にスケール拡大*/
-  }
+  ${({ theme }) => setEffect(theme.hoverEffect)}
 `
