ce13c9ce1ee5093de602c10046b551b66fa67bc5
Kanno Ami (tomixy) <tomix05may@gmail.com>
2022-09-06 11:38:04 +0900

    [Button.Burglarize] effect & custum style反映処理実装

--- packages/ui/src/components/core/Button/styled/burglarize.ts
+++ packages/ui/src/components/core/Button/styled/burglarize.ts
@@ -1,5 +1,113 @@
 import styled, { css } from 'styled-components'
 import { ResetCss } from 'styled-utility-first'
 import { match } from 'ts-pattern'
+import { BurglarizeStyleProps } from '../model/style'
 
-export const injectBurglarizeStyle = css``
+const baseStyle = css`
+  ${ResetCss.button};
+
+  ${({ theme }) => css`
+    --border-radius: ${theme.borderRadiusV + theme.borderRadiusU};
+    --color: ${theme.color};
+    --duration: ${theme.duration}s; // 0.3s
+    --padding: ${theme.paddingV + theme.paddingU};
+    --bg-color: ${theme.bgColor};
+    --offset: ${theme.offset}px;
+  `}
+
+  /*影の基点とするためrelativeを指定*/
+  position: relative;
+  /*ボタンの形状*/
+  text-decoration: none;
+  display: inline-block;
+  text-align: center;
+  background: transparent;
+  border-radius: var(--border-radius);
+  border: solid 1px var(--color);
+  transition: all calc(var(--duration) * 2 / 3) ease;
+
+  /*hoverをした後のボタンの形状*/
+  &:hover {
+    border-color: transparent;
+  }
+
+  /*ボタンの中のテキスト*/
+  & span {
+    position: relative;
+    z-index: 2; /*z-indexの数値をあげて文字を背景よりも手前に表示*/
+    /*テキストの形状*/
+    display: block;
+    padding: var(--padding);
+    background: var(--bg-color);
+    border-radius: var(--border-radius);
+    color: var(--color);
+    /*アニメーションの指定*/
+    transition: all var(--duration) ease;
+  }
+
+  /*影の設定*/
+  &::before {
+    content: '';
+    /*絶対配置で影の位置を決める*/
+    position: absolute;
+    z-index: -1;
+    // TODO shadowPosition
+    /*影の形状*/
+    width: 100%;
+    height: 100%;
+    border-radius: var(--border-radius);
+    background-color: var(--color);
+  }
+
+  &:hover span {
+    background-color: var(--color);
+    color: var(--bg-color);
+    // TODO hoverTransform
+  }
+`
+
+const effectByPushToAs = (pushTo: BurglarizeStyleProps['pushTo']) => {
+  return match(pushTo)
+    .with('bottom', () => {
+      return css`
+        &::before {
+          top: var(--offset);
+          left: 0;
+        }
+
+        &:hover span {
+          transform: translateY(var(--offset));
+        }
+      `
+    })
+    .with('right', () => {
+      return css`
+        &::before {
+          top: var(--offset);
+          left: var(--offset);
+        }
+
+        &:hover span {
+          transform: translate(var(--offset), var(--offset));
+        }
+      `
+    })
+    .with('left', () => {
+      return css`
+        &::before {
+          top: var(--offset);
+          right: var(--offset);
+        }
+
+        &:hover span {
+          transform: translate(calc(-1 * var(--offset)), var(--offset));
+        }
+      `
+    })
+    .otherwise(() => '')
+}
+
+export const injectBurglarizeStyle = css`
+  ${baseStyle}
+  ${({ theme }) => effectByPushToAs(theme.pushTo)}
+`
