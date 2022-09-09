2ad0ed4c5ccb959cdd7e1898d7a6b461b8ea8600
Kanno Ami (tomixy) <tomix05may@gmail.com>
2022-09-09 14:44:33 +0900

    [DropDownNav] 階層に応じた隙間の調整

--- packages/ui/src/components/DropDownNav/styled/nav.ts
+++ packages/ui/src/components/DropDownNav/styled/nav.ts
@@ -2,19 +2,28 @@ import styled from 'styled-components'
 import { ResetCss } from 'styled-utility-first'
 
 export const Nav = styled.nav`
+  --font-size: 1.2rem;
+  --icon-height: 1.5rem;
+  --nav-paddingX: 1rem;
+  --nav-paddingY: 1rem;
+
+  background-color: #4d608b;
+  color: #f5f4f0;
+  padding: var(--nav-paddingY) 0;
+
   /* reset -------------------------------------- */
   & button,
   & a {
     ${ResetCss.button}
     text-decoration: none;
-    color: #4d608b;
-    font-size: 1.2rem;
+    color: inherit;
+    font-size: var(--font-size);
     flex: 1;
   }
 
   & a {
     display: inline-flex;
-    height: 1.5em;
+    height: var(--icon-height);
     align-items: center;
   }
 
@@ -22,11 +31,15 @@ export const Nav = styled.nav`
     list-style: none;
   }
 
+  & > ul {
+    margin: 0;
+  }
+
   /* arrow icon --------------------------------- */
 
   & [aria-haspopup='menu'] svg {
-    width: 1.5em;
-    height: 1.5em;
+    width: var(--icon-height);
+    height: var(--icon-height);
   }
 
   & [aria-haspopup='menu'][aria-expanded='true'] svg {
@@ -43,7 +56,16 @@ export const Nav = styled.nav`
   /*下の階層のulや矢印の基点にするためliにrelativeを指定*/
   & ul li {
     position: relative;
-    height: 1.5rem;
+    height: var(--icon-height);
+  }
+
+  & ul ul li {
+    margin-top: var(--nav-paddingY);
+  }
+
+  /* navBarの真下に来るliはbarと隙間を開ける */
+  & > ul > li > ul > li:first-child {
+    margin-top: calc(var(--nav-paddingY) * 2);
   }
 `
 
@@ -58,12 +80,16 @@ export const Horizontal = styled.div`
     white-space: nowrap;
     overflow: visible;
   }
+
+  & ul li:first-child {
+    margin-top: 0;
+  }
 `
 
 export const TopUl = styled.ul`
   display: flex;
   justify-content: space-between;
   align-items: flex-start;
-  padding-left: 1em;
-  padding-right: 1em;
+  padding-left: var(--nav-paddingX);
+  padding-right: var(--nav-paddingX);
 `
