546be76875e74d68919109bd4ebbafdcec8e09b7
Kanno Ami (tomixy) <tomix05may@gmail.com>
2022-09-13 18:59:56 +0900

    [CodeHighlight] inline-codeとして表示するかどうかをpropsで制御できるように

--- packages/ui/src/components/CodeHighlight/index.tsx
+++ packages/ui/src/components/CodeHighlight/index.tsx
@@ -1,23 +1,30 @@
-import { useEffect } from 'react'
+import { Fragment, useEffect } from 'react'
 import Prism from 'prismjs'
 import { GlobalTheme } from './styled/global'
 
 type CodeHighlightProps = {
   lang: string
   children: string
+  isInline?: boolean
 }
 
-export const CodeHighlight = ({ lang, children }: CodeHighlightProps) => {
+export const CodeHighlight = ({
+  lang,
+  children,
+  isInline = false,
+}: CodeHighlightProps) => {
   useEffect(() => {
     Prism.highlightAll()
   }, [])
 
+  const Wrap = isInline ? Fragment : 'pre'
+
   return (
     <>
       <GlobalTheme />
-      <pre>
+      <Wrap>
         <code className={'language-' + lang}>{children}</code>
-      </pre>
+      </Wrap>
     </>
   )
 }
