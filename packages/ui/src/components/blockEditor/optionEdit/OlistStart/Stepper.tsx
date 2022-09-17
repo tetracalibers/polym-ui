e71d2ee51f2cc8273474f91f7bdf9820d52d5893
Kanno Ami (tomixy) <tomix05may@gmail.com>
2022-09-17 20:37:51 +0900

    [Stepper] box-shadowの調整

--- packages/ui/src/components/blockEditor/optionEdit/OlistStart/Stepper.tsx
+++ packages/ui/src/components/blockEditor/optionEdit/OlistStart/Stepper.tsx
@@ -78,6 +78,8 @@ const Input = styled(NumberInput)`
   padding: 1.8rem 1rem 0.6rem;
   font-size: 1rem;
   width: 100%;
+  box-shadow: rgba(50, 50, 105, 0.15) 0px 2px 5px 0px,
+    rgba(0, 0, 0, 0.05) 0px 1px 1px 0px;
 
   &:focus {
     outline: none;
