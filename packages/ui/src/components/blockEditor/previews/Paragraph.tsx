467e5b8516922268d2888bd9404da54f9a6d3aff
Kanno Ami (tomixy) <tomix05may@gmail.com>
2022-09-18 00:18:31 +0900

    [BlockEditor] Paragarphのpreviewの改行対応と均等配置など

--- packages/ui/src/components/blockEditor/previews/Paragraph.tsx
+++ packages/ui/src/components/blockEditor/previews/Paragraph.tsx
@@ -1,11 +1,29 @@
-import styled from 'styled-components'
+import styled, { css } from 'styled-components'
 
 export type ParagraphBlockProps = {
   children: string
   isInline?: boolean
 }
 
+const contentStyle = css`
+  margin: 0;
+  line-height: 1.5em;
+  /* 改行 */
+  overflow-wrap: break-word;
+  word-wrap: break-word;
+  /* 均等配置（両端揃え） */
+  text-align: justify;
+  text-justify: inter-ideograph;
+  /* ハイフン */
+  hyphens: auto;
+`
+
+const blockParagraph = styled.p`
+  ${contentStyle}
+`
+
 const inlineParagraph = styled.p`
+  ${contentStyle}
   display: inline;
 `
 
@@ -13,7 +31,7 @@ export const ParagraphPreview = ({
   children,
   isInline = false,
 }: ParagraphBlockProps) => {
-  const Tag = isInline ? inlineParagraph : 'p'
+  const Tag = isInline ? inlineParagraph : blockParagraph
 
   return <Tag>{children}</Tag>
 }
