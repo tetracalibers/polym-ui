9f25363d491375aaeec3563d40458f54b4e6e454
Kanno Ami (tomixy) <tomix05may@gmail.com>
2022-09-19 18:41:41 +0900

    [BlockEditor] Tabのデザイン調整

--- packages/ui/src/components/blockEditor/blocks/ImageBlock/index.tsx
+++ packages/ui/src/components/blockEditor/blocks/ImageBlock/index.tsx
@@ -1,4 +1,5 @@
 import { useContext } from 'react'
+import styled from 'styled-components'
 import { BlockEditorContext } from '../..'
 import { Tab } from '../../../Tab'
 import { FormatArgs } from '../../module/FormatArgs'
@@ -6,6 +7,33 @@ import { UpdateAction } from '../../module/reducer'
 import { GroupPanel } from '../GroupPanel'
 import { Dropzone } from './Dropzone'
 import { UrlInput } from './UrlInput'
+
+const Container = styled(GroupPanel)`
+  /* tabのスタイル上書き */
+  & ul[role='tablist'] {
+    --duration: 0.3s;
+
+    justify-content: start;
+    margin: 0;
+  }
+
+  & ul[role='tablist'] > li[data-active] > a {
+    padding: 0.75rem 1.75rem;
+    border-radius: 1rem 1rem 0 0;
+    color: #4d608b;
+    text-transform: uppercase;
+    letter-spacing: 2px;
+  }
+
+  & ul[role='tablist'] > li[data-active] > a::after {
+    background: linear-gradient(to right, #fbc2eb, #a6c1ee);
+  }
+
+  & ul[role='tablist'] > li[data-active='false'] > a {
+    box-shadow: inset #3f3f3f33 0px 1px 3px 0px;
+    background-color: rgba(255, 255, 255, 0.25);
+  }
+`
 
 type ImageBlockProps = {
   id: string
@@ -27,15 +55,15 @@ export const ImageBlock = ({ id, value }: ImageBlockProps) => {
   }
 
   return (
-    <GroupPanel>
+    <Container>
       <Tab titleTabListTheme={'Fill'} hoverEffect='fillFromUnderline'>
-        <Tab.Panel title='Upload'>
+        <Tab.Panel title='File'>
           <Dropzone updateFn={updateFn} />
         </Tab.Panel>
-        <Tab.Panel title='URL Input'>
+        <Tab.Panel title='URL'>
           <UrlInput id={id} value={value.url} />
         </Tab.Panel>
       </Tab>
-    </GroupPanel>
+    </Container>
   )
 }
